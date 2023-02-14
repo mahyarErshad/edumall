182860618d4b2c387dcc61fad196329ded169227
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-14 15:29:56 +0330

    Fixed a bug

--- src/Components/Header/Header/Header.jsx
+++ src/Components/Header/Header/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
         <NavLink text="ویژگی ها" href="#features" />
         <NavLink text="ویدئوهای آموزشی" href="#tutorials" />
         <NavLink text="دموها" href="#demos" />
-        <NavLink text="دوره ها" href="courses" />
+        <NavLink text="دوره ها" href="#courses" />
       </>
     );
   };
