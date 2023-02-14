47c689e4e0866160379c6658f8137483687f7ee2
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-14 15:46:56 +0330

    Font size 1 rem

--- src/Components/Utils/Buttons/BuyProButton/BuyProButton.tsx
+++ src/Components/Utils/Buttons/BuyProButton/BuyProButton.tsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.css";
 function BuyProButton() {
   return (
     <div className="h-full flex-center lg:mt-[0.375rem] max-lg:order-2">
-      <a className={`${styles.proButton} max-sm:hidden text-sm font-bold`} href="#app">
+      <a className={`${styles.proButton} max-sm:hidden font-bold`} href="#app">
         خرید نسخه حرفه ای
       </a>
     </div>
