328421dfc68cb7553326a4eb4e358d660c6f0d16
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-14 09:00:34 +0330

    Imported MainHeader

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import MainHeader from "./Components/Header/MainHeader/MainHeader";
 
 function App() {
-  return <>
-  <MainHeader />
-  </>;
+  return (
+    <>
+      <MainHeader />
+    </>
+  );
 }
 
 export default App;
