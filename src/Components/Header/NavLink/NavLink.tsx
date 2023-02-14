502d35282cd46a8bcb02b22c5c0d9a2f756c986b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-14 10:59:07 +0330

    Removed hover effect

--- src/Components/Header/NavLink/NavLink.tsx
+++ src/Components/Header/NavLink/NavLink.tsx
@@ -9,7 +9,7 @@ interface props {
 function NavLink(props: props) {
   const { href, text } = props;
   return (
-    <li className={`${styles.wrapper} cursor-pointer max-lg:before:hidden text-white max-lg:hover:text-primary`}>
+    <li className={`${styles.wrapper} cursor-pointer max-lg:before:hidden text-white`}>
       <a className="text-inherit whitespace-nowrap text-sm font-semibold transition-all duration-300 inline-block max-lg:my-2" href={href}>
         {text}
       </a>
