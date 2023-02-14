274170cf291baaa3018c4f7a75b529e65133b83d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-14 15:43:18 +0330

    Removed margin left

--- src/Components/Utils/HeaderUtils/Logo/Logo.jsx
+++ src/Components/Utils/HeaderUtils/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import logo from "../../../../assets/images/logo.png";
 function Logo() {
   return (
     <div onClick={() => window.scrollTo({ top: 0 })} className="flex-center h-full cursor-pointer">
-      <img className="max-w-[9.375rem] lg:ml-4" src={logo} alt="Logo" />
+      <img className="max-w-[9.375rem]" src={logo} alt="Logo" />
     </div>
   );
 }
