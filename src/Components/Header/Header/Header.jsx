6a6d96122f273758948148c2978531c4821d006f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-14 10:24:46 +0330

    Changed some styles

--- src/Components/Header/Header/Header.jsx
+++ src/Components/Header/Header/Header.jsx
@@ -19,10 +19,13 @@ function Header() {
   };
   return (
     <header className="min-h-[4.375rem] w-full py-2 bg-transparent z-50">
-      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 px-4">
+      <div className="max-w-[93.75rem] mx-auto sm:px-6 lg:px-8 px-4">
         <div className="flex items-center lg:flex-row-reverse justify-between h-16">
-          <div className="-mr-2 flex lg:hidden">
+          <div className="-mr-2 flex lg:hidden mt-[0.375rem]">
             <BurgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
+          </div>
+          <div className="h-full flex-center lg:mt-[0.375rem]">
+            <button>خرید نسخه حرفه ای</button>
           </div>
           <div className="flex items-center">
             <div className="hidden lg:block">
