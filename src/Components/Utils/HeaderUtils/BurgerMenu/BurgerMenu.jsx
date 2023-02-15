4377873750aabbb113270e077d3f1dbac1dbdbaa
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-15 12:36:20 +0330

    Added animation for both conditions

--- src/Components/Utils/HeaderUtils/BurgerMenu/BurgerMenu.jsx
+++ src/Components/Utils/HeaderUtils/BurgerMenu/BurgerMenu.jsx
@@ -5,9 +5,10 @@ import MyContext from "../../../../Context/MyContext";
 
 function BurgerMenu({ close }) {
   const Context = useContext(MyContext);
+  const animation = close ? "close-button-animation" : "open-button-animation";
   return (
     <div className="-mr-2 flex lg:hidden mt-[0.375rem] relative">
-      <button onClick={() => Context.setIsOpen((prev) => !prev)} type="button" className={`${close && "close-button-animation"} bg-transparent -mt-[0.375rem] flex-center p-2 rounded-md text-black transition-all duration-300`} aria-controls="mobile-menu" aria-expanded="false">
+      <button onClick={() => Context.setIsOpen((prev) => !prev)} type="button" className={`${animation} bg-transparent -mt-[0.375rem] flex-center p-2 rounded-md text-black transition-all duration-300`} aria-controls="mobile-menu" aria-expanded="false">
         {close ? <FontAwesomeIcon size="2xl" color="#111" icon={faTimes} /> : <FontAwesomeIcon color="#fff" size="xl" icon={faBars} />}
       </button>
     </div>
