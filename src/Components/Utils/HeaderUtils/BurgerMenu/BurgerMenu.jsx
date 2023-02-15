713cb6b2bb89be25212368bf84297a3367f9dc27
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-15 10:49:00 +0330

    Worked on burgerMenu button

--- src/Components/Utils/HeaderUtils/BurgerMenu/BurgerMenu.jsx
+++ src/Components/Utils/HeaderUtils/BurgerMenu/BurgerMenu.jsx
@@ -1,3 +1,5 @@
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useContext } from "react";
 import MyContext from "../../../../Context/MyContext";
 
@@ -5,9 +7,11 @@ function BurgerMenu() {
   const Context = useContext(MyContext);
   return (
     <div className="-mr-2 flex lg:hidden mt-[0.375rem]">
-      <button onClick={() => Context.setIsOpen((prev) => !prev)} type="button" className="bg-transparent flex-center p-2 rounded-md hover:text-primary transition-all duration-300" aria-controls="mobile-menu" aria-expanded="false">
-        <Font
-      </button>
+      {!Context.isOpen && (
+        <button onClick={() => Context.setIsOpen((prev) => !prev)} type="button" className="icon-button bg-transparent flex-center p-2 rounded-md hover:text-primary transition-all duration-300" aria-controls="mobile-menu" aria-expanded="false">
+          <FontAwesomeIcon className="w-[2rem] h-[1.5rem]" icon={faBars} />
+        </button>
+      )}
     </div>
   );
 }
