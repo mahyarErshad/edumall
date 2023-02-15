501c5a687a4bcaefd0694afca0488661395ba28f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-15 10:54:53 +0330

    Added dark logo image

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,13 +1,20 @@
 import React, { useContext } from "react";
 import MyContext from "../../Context/MyContext";
+import BurgerMenu from "../Utils/HeaderUtils/BurgerMenu/BurgerMenu";
+import Logo from "../Utils/HeaderUtils/Logo/Logo";
 
 function SideBar() {
   const context = useContext(MyContext);
   const width = context.isOpen ? "max-md:w-[20.625rem] md:w-[22.5rem]" : "w-0";
 
-  return <div className={`${width} lg:hidden transition-all duration-500 relative`}>
-    
-  </div>;
+  return (
+    <div className={`${width} lg:hidden transition-all duration-500`}>
+      <header className="min-h-[5rem] flex justify-between px-4">
+        <Logo />
+        <BurgerMenu />
+      </header>
+    </div>
+  );
 }
 
 export default SideBar;
