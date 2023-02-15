5c93fc49eb5c1ed771e2f4ada482e761fae1e70b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-15 10:56:02 +0330

    Logo can be dark

--- src/Components/Utils/HeaderUtils/Logo/Logo.jsx
+++ src/Components/Utils/HeaderUtils/Logo/Logo.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import logo from "../../../../assets/images/logo.png";
+import darkLogo from "../../../../assets/images/dark-logo.png";
 
-function Logo() {
+function Logo({ dark }) {
   return (
     <div onClick={() => window.scrollTo({ top: 0 })} className="flex-center h-full cursor-pointer">
-      <img className="max-w-[9.375rem]" src={logo} alt="Logo" />
+      <img className="max-w-[9.375rem]" src={dark ? darkLogo : logo} alt="Logo" />
     </div>
   );
 }
