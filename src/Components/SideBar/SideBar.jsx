dbc3b029abe3639f31fcc8fad204b0a4abf17ed1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-15 10:49:42 +0330

    Removed condition from here

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -5,7 +5,9 @@ function SideBar() {
   const context = useContext(MyContext);
   const width = context.isOpen ? "max-md:w-[20.625rem] md:w-[22.5rem]" : "w-0";
 
-  return <div className={`${width} lg:hidden transition-all duration-500`}>SideBar</div>;
+  return <div className={`${width} lg:hidden transition-all duration-500 relative`}>
+    
+  </div>;
 }
 
 export default SideBar;
--- src/Components/Utils/HeaderUtils/BurgerMenu/BurgerMenu.jsx
+++ src/Components/Utils/HeaderUtils/BurgerMenu/BurgerMenu.jsx
@@ -7,11 +7,9 @@ function BurgerMenu() {
   const Context = useContext(MyContext);
   return (
     <div className="-mr-2 flex lg:hidden mt-[0.375rem]">
-      {!Context.isOpen && (
         <button onClick={() => Context.setIsOpen((prev) => !prev)} type="button" className="icon-button bg-transparent flex-center p-2 rounded-md hover:text-primary transition-all duration-300" aria-controls="mobile-menu" aria-expanded="false">
           <FontAwesomeIcon className="w-[2rem] h-[1.5rem]" icon={faBars} />
         </button>
-      )}
     </div>
   );
 }
