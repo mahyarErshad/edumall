680c901e1a589fa1b6fa81a24112bc4e54c609c4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-15 11:53:10 +0330

    Sidebar has background image and gradient

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -2,21 +2,19 @@ import React, { useContext } from "react";
 import MyContext from "../../Context/MyContext";
 import BurgerMenu from "../Utils/HeaderUtils/BurgerMenu/BurgerMenu";
 import Logo from "../Utils/HeaderUtils/Logo/Logo";
-import styles from "./sidebar.module.css"
+import styles from "./sidebar.module.css";
 
 function SideBar() {
   const context = useContext(MyContext);
   const width = context.isOpen ? "max-md:w-[20.625rem] md:w-[22.5rem]" : "w-0";
 
   return (
-    <div className={`${width} lg:hidden transition-all duration-500`}>
-      <header className="min-h-[5rem] flex justify-between items-center px-4">
+    <div className={`${width} h-full lg:hidden transition-all duration-500 ${styles.grid}`}>
+      <header className="min-h-[5rem] w-full flex justify-between items-center px-4">
         <BurgerMenu close />
         <Logo dark />
       </header>
-      <main className={styles.main}>
-
-      </main>
+      <main className={styles.main}></main>
     </div>
   );
 }
