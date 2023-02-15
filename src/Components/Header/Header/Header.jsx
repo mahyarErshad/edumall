8085561c04f665d01be9e67ae5426b5e9d7c5918
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-15 10:50:21 +0330

    Import context

--- src/Components/Header/Header/Header.jsx
+++ src/Components/Header/Header/Header.jsx
@@ -1,10 +1,12 @@
-import React, { memo } from "react";
+import React, { memo, useContext } from "react";
 import BurgerMenu from "../../Utils/HeaderUtils/BurgerMenu/BurgerMenu";
 import BuyProButton from "../../Utils/Buttons/BuyProButton/BuyProButton";
 import HeaderNavigationSection from "../../Utils/HeaderUtils/HeaderNavigationSection/HeaderNavigationSection";
 import Logo from "../../Utils/HeaderUtils/Logo/Logo";
+import MyContext from "../../../Context/MyContext";
 
 function Header() {
+  const context = useContext(MyContext)
   return (
     <header className="min-h-[4.375rem] w-full py-2 bg-transparent z-50">
       <div className="max-w-[93.75rem] mx-auto sm:px-6 lg:px-8 px-4">
