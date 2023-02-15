645a38e28af0cd74af137baebfc2f0de11f16df5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-15 10:31:51 +0330

    SideBar opens and closes well

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
+import MyContext from "../../Context/MyContext";
 
 function SideBar() {
-  return <div>SideBar</div>;
+  const context = useContext(MyContext);
+  const width = context.isOpen ? "max-md:w-[20.625rem] md:w-[22.5rem]" : "w-0";
+
+  return <div className={`${width} lg:hidden transition-all duration-500`}>SideBar</div>;
 }
 
 export default SideBar;
