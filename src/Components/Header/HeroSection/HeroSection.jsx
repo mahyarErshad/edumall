c9a770c18f87f5a710dc904bc0d1b2f61698d362
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-19 11:27:02 +0330

    Typing animation is added

--- src/Components/Header/HeroSection/HeroSection.jsx
+++ src/Components/Header/HeroSection/HeroSection.jsx
@@ -7,10 +7,10 @@ function HeroSection() {
   return (
     <section className="relative w-full bg-primary min-h-screen">
       <Header />
-      <div className="relative max-lg:flex flex-col gap-4 max-lg:pt-8">
+      <div className="relative max-lg:flex flex-col max-lg:pt-8">
         <div className="font-medium md:text-5xl max-md:text-3xl text-white lg:absolute lg:right-[50px] lg:top-[100px] z-10 lg:max-w-[50%] max-lg:mb-8 max-lg:text-center flex flex-col gap-6">
           <h1>قالب وردپرسی آموزشی همه کاره برای</h1>
-          <TypeAnimation sequence={["Delete faster or slower by setting speed.", 2000, ""]} speed={75} deletionSpeed={99} wrapper="h2" repeat={Infinity} />
+          <TypeAnimation sequence={["دوره های آنلاین", 2000, "مراکز آموزشی", 2000, "مدارس", 2000]} speed={75} deletionSpeed={99} wrapper="h2" className="text-secondary font-bold" repeat={Infinity} />
         </div>
         <img src={image} alt="Edumall" className="lg:absolute lg:left-[-7rem] lg:top-[3.125rem] mirror" />
       </div>
