09c14dac9267b0202ce010bb5fcb09e76cca791a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-19 11:09:44 +0330

    Text section is responsive

--- src/Components/Header/HeroSection/HeroSection.jsx
+++ src/Components/Header/HeroSection/HeroSection.jsx
@@ -6,7 +6,10 @@ function HeroSection() {
   return (
     <section className="relative w-full bg-primary min-h-screen">
       <Header />
-      <img src={image} alt="Edumall" className="absolute lg:left-[-4.375rem] lg:top-[3.125rem] mirror max-md:hidden" />
+      <div className="relative max-lg:flex flex-col gap-4 max-lg:pt-8">
+        <h1 className="font-medium md:text-5xl max-md:text-3xl text-white lg:absolute lg:right-[50px] lg:top-[100px] z-10 lg:max-w-[50%] max-lg:mb-8 max-lg:text-center">قالب وردپرسی آموزشی همه کاره برای</h1>
+        <img src={image} alt="Edumall" className="lg:absolute lg:left-[-7rem] lg:top-[3.125rem] mirror" />
+      </div>
     </section>
   );
 }
