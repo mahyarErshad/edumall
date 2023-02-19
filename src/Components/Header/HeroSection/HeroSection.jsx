e91c073dd52834dcb4faf32d375fc9509c4c94ed
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-19 09:53:27 +0330

    Added mirror class

--- src/Components/Header/HeroSection/HeroSection.jsx
+++ src/Components/Header/HeroSection/HeroSection.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../Header/Header";
+import image from "../../../assets/images/hero-image.png";
 
 function HeroSection() {
   return (
     <section className="relative w-full bg-primary min-h-screen">
       <Header />
+      <img src={image} alt="Edumall" className="absolute w-full lg:right-[-4.375rem] lg:top-[3.125rem]" />
     </section>
   );
 }
