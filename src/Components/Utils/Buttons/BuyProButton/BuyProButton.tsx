9f046a4f928d9d2efcdaf5dcf4f97d4402c416d8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-22 13:24:57 +0330

    Removed margin top

--- src/Components/Utils/Buttons/BuyProButton/BuyProButton.tsx
+++ src/Components/Utils/Buttons/BuyProButton/BuyProButton.tsx
@@ -7,7 +7,7 @@ interface props {
 
 function BuyProButton(props: props) {
   return (
-    <div className={`h-full ${props.className} lg:mt-[0.375rem] max-lg:order-2`}>
+    <div className={`h-full ${props.className} max-lg:order-2`}>
       <a className={`${styles.proButton} font-bold w-full`} href="#app">
         خرید نسخه حرفه ای
       </a>
