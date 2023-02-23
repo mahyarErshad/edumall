c69c58b372d5f6d89f542a6137de500c57e1a35b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-23 10:08:55 +0330

    Created WhyEdumallCards component

--- src/Components/Body/WhyEdumall/WhyEdumallCards/WhyEdumallCards.jsx
+++ src/Components/Body/WhyEdumall/WhyEdumallCards/WhyEdumallCards.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import styles from "./styles.module.css"
+import React from "react";
+import styles from "./styles.module.css";
 
 function WhyEdumallCards() {
-  return (
-    <figure className={`${styles.wrapper} p-8 bg-[#F8F7F3] rounded-[10px] hover:bg-[#FFFEFA]`}>
-
-    </figure>
-  )
+  return <figure className={`${styles.wrapper} p-8 bg-[#F8F7F3] rounded-[10px] hover:bg-[#FFFEFA] transition-all duration-300`}>
+sfsdgsdfgvfe
+  </figure>;
 }
 
-export default WhyEdumallCards
+export default WhyEdumallCards;
