fd2a9f53369eee7e286392dda970b3eb3b1055e1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-23 09:20:25 +0330

    Imported styles

--- src/Components/Body/WhyEdumall/WhyEdumallCards/WhyEdumallCards.jsx
+++ src/Components/Body/WhyEdumall/WhyEdumallCards/WhyEdumallCards.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from "./styles.module.css"
 
 function WhyEdumallCards() {
   return (
     <figure className="p-8 bg-[#F8F7F3] rounded-[10px] hover:bg-[#FFFEFA]">
-        
+
     </figure>
   )
 }
