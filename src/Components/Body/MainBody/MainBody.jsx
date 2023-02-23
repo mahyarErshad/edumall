6757fc7ac1af7bb89ec9b7a0e7a0a3b7326eab2a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-23 08:18:02 +0330

    Imported WhyEdumall

--- src/Components/Body/MainBody/MainBody.jsx
+++ src/Components/Body/MainBody/MainBody.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../../Header/HeroSection/HeroSection'
+import WhyEdumall from '../WhyEdumall/WhyEdumall'
 
 function MainBody() {
   return (
     <>
       <HeroSection />
+      <WhyEdumall />
     </>
   )
 }
