288f86ff37ad3a6ab445379ac0bcee816260011f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-23 11:20:50 +0330

    Changed the width

--- src/Components/Body/WhyEdumall/WhyEdumallCards/WhyEdumallCards.jsx
+++ src/Components/Body/WhyEdumall/WhyEdumallCards/WhyEdumallCards.jsx
@@ -4,7 +4,7 @@ import image from "../../../../assets/images/why-edumall-cards/1.png";
 
 function WhyEdumallCards() {
   return (
-    <figure className={`${styles.wrapper} p-8 bg-[#F8F7F3] rounded-[10px] hover:bg-[#FFFEFA] transition-all duration-300 min-h-[13.5625rem] max-md:w-full md:w-[30%]`}>
+    <figure className={`${styles.wrapper} p-8 bg-[#F8F7F3] rounded-[10px] hover:bg-[#FFFEFA] transition-all duration-300 min-h-[13.5625rem] max-lg:w-full lg:w-[30%]`}>
       <img src={image} alt="" className={`${styles.image}`} />
       <h3 className={`${styles.title}`}>test tedtda saef</h3>
       <p className={`${styles.description}`}>test tedtda saefsragsad sadsghdshbdsh bgdshbsxfzhge tdsgbrdhbdfg hsndthgbhrhdf sgfes dhbsgd ngrsth bgshd sdhg srgd dfrh</p>
