9889d2f69ab0d7eb10b383b50a2ae6cd12d5e931
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-23 11:18:25 +0330

    px = 1rem

--- src/Components/Body/WhyEdumall/WhyEdumall.jsx
+++ src/Components/Body/WhyEdumall/WhyEdumall.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import WhyEdumallCards from "./WhyEdumallCards/WhyEdumallCards";
 
 function WhyEdumall() {
-  return <section className="w-full max-lg:pt-8 lg:pt-80 flex-center bg-white max-lg:gap-4 lg:gap-8 flex-wrap">
+  return <section className="w-full max-lg:pt-8 lg:pt-80 flex-center bg-white max-lg:gap-4 lg:gap-8 flex-wrap px-4">
   <WhyEdumallCards />
   <WhyEdumallCards />
   <WhyEdumallCards />
