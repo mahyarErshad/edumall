34619494724103ab7461df71b9f533cd8ad8e2c6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-23 11:18:10 +0330

    Worked on cards to be responsive

--- src/Components/Body/WhyEdumall/WhyEdumall.jsx
+++ src/Components/Body/WhyEdumall/WhyEdumall.jsx
@@ -4,6 +4,8 @@ import WhyEdumallCards from "./WhyEdumallCards/WhyEdumallCards";
 function WhyEdumall() {
   return <section className="w-full max-lg:pt-8 lg:pt-80 flex-center bg-white max-lg:gap-4 lg:gap-8 flex-wrap">
   <WhyEdumallCards />
+  <WhyEdumallCards />
+  <WhyEdumallCards />
   </section>;
 }
 
