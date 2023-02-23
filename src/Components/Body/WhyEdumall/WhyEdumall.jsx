05305e96fb44be71240cac01bdd748c498017654
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-23 10:09:14 +0330

    Imported WhyEdumallCards

--- src/Components/Body/WhyEdumall/WhyEdumall.jsx
+++ src/Components/Body/WhyEdumall/WhyEdumall.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import WhyEdumallCards from "./WhyEdumallCards/WhyEdumallCards";
 
 function WhyEdumall() {
   return <section className="w-full max-lg:pt-8 lg:pt-80 flex-center bg-white max-lg:gap-4 lg:gap-8 flex-wrap">
-  <figure className="p-8 bg-[#F8F7F3] rounded-[10px] hover:bg-[#FFFEFA]"></figure>
+  <WhyEdumallCards />
   </section>;
 }
 
