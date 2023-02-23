375bb37b648019c26a6b544d37ac94b2a823f2eb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-23 09:19:25 +0330

    Created a component for WhyEdumallCards

--- src/Components/Body/WhyEdumall/WhyEdumall.jsx
+++ src/Components/Body/WhyEdumall/WhyEdumall.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 function WhyEdumall() {
-  return <section className="w-full max-lg:pt-8 lg:pt-80 flex-center bg-white">
-    
+  return <section className="w-full max-lg:pt-8 lg:pt-80 flex-center bg-white max-lg:gap-4 lg:gap-8 flex-wrap">
+  <figure className="p-8 bg-[#F8F7F3] rounded-[10px] hover:bg-[#FFFEFA]"></figure>
   </section>;
 }
 
---
+++ src/Components/Body/WhyEdumall/WhyEdumallCards/WhyEdumallCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WhyEdumallCards() {
+  return (
+    <figure className="p-8 bg-[#F8F7F3] rounded-[10px] hover:bg-[#FFFEFA]">
+        
+    </figure>
+  )
+}
+
+export default WhyEdumallCards
