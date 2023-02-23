68c5b8b068fa0f5a2d2cfb654b55a5bdd3c810e6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-23 10:13:34 +0330

    Added a new image

--- src/Components/Body/WhyEdumall/WhyEdumallCards/WhyEdumallCards.jsx
+++ src/Components/Body/WhyEdumall/WhyEdumallCards/WhyEdumallCards.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import styles from "./styles.module.css";
 
 function WhyEdumallCards() {
-  return <figure className={`${styles.wrapper} p-8 bg-[#F8F7F3] rounded-[10px] hover:bg-[#FFFEFA] transition-all duration-300`}>
-  
-  </figure>;
+  return (
+    <figure className={`${styles.wrapper} p-8 bg-[#F8F7F3] rounded-[10px] hover:bg-[#FFFEFA] transition-all duration-300`}>
+      <img src="" alt="" className={`${styles.image}`} />
+    </figure>
+  );
 }
 
 export default WhyEdumallCards;
