9d3ac4969204a143a48b6e3d657f5cf9f89c8afd
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-25 09:19:47 +0330

    Removed height

--- src/Components/Body/WhyEdumall/WhyEdumallCards/WhyEdumallCards.tsx
+++ src/Components/Body/WhyEdumall/WhyEdumallCards/WhyEdumallCards.tsx
@@ -9,7 +9,7 @@ interface props {
 
 function WhyEdumallCards(props: props) {
   return (
-    <figure className={`${styles.wrapper} p-8 bg-[#F8F7F3] rounded-[10px] hover:bg-[#FFFEFA] transition-all duration-300 min-h-[13.5625rem] max-lg:w-full lg:w-[30%]`}>
+    <figure className={`${styles.wrapper} bg-[#F8F7F3] px-6 py-12 rounded-[10px] hover:bg-[#FFFEFA] transition-all duration-300 max-lg:w-full lg:w-[30%]`}>
       <img src={props.image} alt={props.title} className={`${styles.image}`} />
       <h3 className={`${styles.title}`}>{props.title}</h3>
       <p className={`${styles.description}`}>{props.description}</p>
