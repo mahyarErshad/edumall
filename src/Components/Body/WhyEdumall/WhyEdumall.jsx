198d97f1e10d8826e69b77b7c73f93089ea8ac67
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-25 09:00:59 +0330

    Passed the images

--- src/Components/Body/WhyEdumall/WhyEdumall.jsx
+++ src/Components/Body/WhyEdumall/WhyEdumall.jsx
@@ -6,9 +6,9 @@ import image3 from "../../../assets/images/why-edumall-cards/3.png"
 
 function WhyEdumall() {
   return <section className="w-full max-lg:pt-8 lg:pt-[15%] flex-center bg-white max-lg:gap-4 lg:gap-8 flex-wrap px-4">
-  <WhyEdumallCards />
-  <WhyEdumallCards />
-  <WhyEdumallCards />
+  <WhyEdumallCards image={image1} />
+  <WhyEdumallCards image={image2} />
+  <WhyEdumallCards image={image3} />
   </section>;
 }
 
