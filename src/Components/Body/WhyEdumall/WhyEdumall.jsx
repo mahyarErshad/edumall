2efdac9faaf3e721bb438eae3e74a9be333afa0c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-25 09:38:10 +0330

    Added h3

--- src/Components/Body/WhyEdumall/WhyEdumall.jsx
+++ src/Components/Body/WhyEdumall/WhyEdumall.jsx
@@ -6,12 +6,13 @@ import image3 from "../../../assets/images/why-edumall-cards/3.png";
 
 function WhyEdumall() {
   return (
-    <section className="w-full max-lg:pt-8 lg:pt-[15%] flex-center bg-white px-4">
-      <div className="flex-center w-full max-lg:gap-4 lg:gap-8 flex-wrap">
+    <section className="w-full max-lg:pt-8 lg:pt-[15%] flex-center bg-white px-4 flex-col">
+      <div className="flex-center w-full max-lg:gap-4 lg:gap-8 flex-wrap max-lg:mb-24 lg:mb-28">
         <WhyEdumallCards image={image1} title="آموزش دهنده قدرتمند LMS داخلی" description="یک راه حل هوشمند، ساده و کامل LMS برای ساخت دوره ها و مدیریت مرکز آموزشی شما" />
         <WhyEdumallCards image={image2} title="سازنده دوره های مقدماتی گسترده" description="دوره‌های مدبرانه و آزمون ایجاد کنید. همه چیز را با کشیدن و رها کردن کنترل کنید" />
         <WhyEdumallCards image={image3} title="طرح های نوین و کاربردی" description="یک راه حل هوشمند، ساده و کامل LMS برای ساخت دوره ها و مدیریت مرکز آموزشی شما" />
       </div>
+      <h3>چرا اجومال را <span>انتخاب</span> کنیم</h3>
     </section>
   );
 }
