c878836f540d3b97b28bde49da57f0e734f463ed
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-25 08:57:40 +0330

    Cards are dynamic

--- src/Components/Body/WhyEdumall/WhyEdumallCards/WhyEdumallCards.tsx
+++ src/Components/Body/WhyEdumall/WhyEdumallCards/WhyEdumallCards.tsx
@@ -11,9 +11,9 @@ interface props {
 function WhyEdumallCards(props: props) {
   return (
     <figure className={`${styles.wrapper} p-8 bg-[#F8F7F3] rounded-[10px] hover:bg-[#FFFEFA] transition-all duration-300 min-h-[13.5625rem] max-lg:w-full lg:w-[30%]`}>
-      <img src={image} alt="" className={`${styles.image}`} />
-      <h3 className={`${styles.title}`}>test tedtda saef</h3>
-      <p className={`${styles.description}`}>test tedtda saefsragsad sadsghdshbdsh bgdshbsxfzhge tdsgbrdhbdfg hsndthgbhrhdf sgfes dhbsgd ngrsth bgshd sdhg srgd dfrh</p>
+      <img src={props.image} alt={props.title} className={`${styles.image}`} />
+      <h3 className={`${styles.title}`}>{props.title}</h3>
+      <p className={`${styles.description}`}>{props.description}</p>
     </figure>
   );
 }
