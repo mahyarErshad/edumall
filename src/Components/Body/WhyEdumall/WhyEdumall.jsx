1a817f14237d955ef8bd9e8b32d38d71909f5af6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-25 09:00:32 +0330

    Imported Image3

--- src/Components/Body/WhyEdumall/WhyEdumall.jsx
+++ src/Components/Body/WhyEdumall/WhyEdumall.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import WhyEdumallCards from "./WhyEdumallCards/WhyEdumallCards";
 import image1 from "../../../assets/images/why-edumall-cards/1.jpg"
 import image2 from "../../../assets/images/why-edumall-cards/2.png"
+import image3 from "../../../assets/images/why-edumall-cards/3.png"
 
 function WhyEdumall() {
   return <section className="w-full max-lg:pt-8 lg:pt-[15%] flex-center bg-white max-lg:gap-4 lg:gap-8 flex-wrap px-4">
