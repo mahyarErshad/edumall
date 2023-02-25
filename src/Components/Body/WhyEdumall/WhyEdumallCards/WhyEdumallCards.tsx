6fda64f8fce58228d341c7d93f0149dd9c0d4d93
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-25 09:26:55 +0330

    Added min and max height for lg screens

--- src/Components/Body/WhyEdumall/WhyEdumallCards/WhyEdumallCards.tsx
+++ src/Components/Body/WhyEdumall/WhyEdumallCards/WhyEdumallCards.tsx
@@ -9,7 +9,7 @@ interface props {
 
 function WhyEdumallCards(props: props) {
   return (
-    <figure className={`${styles.wrapper} bg-[#F8F7F3] px-6 py-12 rounded-[10px] hover:bg-[#FFFEFA] transition-all duration-300 max-lg:w-full lg:w-[30%]`}>
+    <figure className={`${styles.wrapper} bg-[#F8F7F3] px-6 py-12 rounded-[10px] hover:bg-[#FFFEFA] transition-all duration-300 max-lg:w-full lg:w-[30%] lg:min-h-[12.5rem] lg:max-h-[16.875rem]`}>
       <img src={props.image} alt={props.title} className={`${styles.image}`} />
       <h3 className={`${styles.title}`}>{props.title}</h3>
       <p className={`${styles.description}`}>{props.description}</p>
