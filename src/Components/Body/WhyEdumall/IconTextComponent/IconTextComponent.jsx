93c89e94eeb2dc41721bc6a64fc192fe8cbad3ab
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-26 09:16:04 +0330

    Cards are dynamic

---
+++ src/Components/Body/WhyEdumall/IconTextComponent/IconTextComponent.jsx
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+function IconTextComponent() {
+  return (
+    <div className="flex-center gap-4 max-md:w-[30%] md:w-full min-w-[24.375rem] py-2 px-4 bg-transparent">
+      <FontAwesomeIcon color="#066cc9" icon={props.icon} />
+      <p className="text-[#9B9B9B] text-base font-medium">{props.text}</p>
+    </div>
+  );
+}
+
+export default IconTextComponent;
