dad13276e2ad4adba1e8fd2de859cf3917a6e11d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-26 09:22:21 +0330

    Catching props

--- src/Components/Body/WhyEdumall/IconTextComponent/IconTextComponent.jsx
+++ src/Components/Body/WhyEdumall/IconTextComponent/IconTextComponent.jsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-function IconTextComponent() {
+function IconTextComponent(props) {
   return (
     <div className="flex-center gap-4 max-md:w-[30%] md:w-full min-w-[24.375rem] py-2 px-4 bg-transparent">
       <FontAwesomeIcon color="#066cc9" icon={props.icon} />
