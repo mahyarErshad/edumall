63dd01c48fbf30ff1d4444eb2944aa92348522ec
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-27 09:42:40 +0330

    Changed justify to start

--- src/Components/Body/WhyEdumall/IconTextComponent/IconTextComponent.jsx
+++ src/Components/Body/WhyEdumall/IconTextComponent/IconTextComponent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function IconTextComponent(props) {
   return (
-    <div className="flex items-center lg:justify-center max-lg:justify-start gap-4 lg:w-[31%] md:w-[48%] max-md:w-full py-2 px-4 bg-transparent">
+    <div className="flex items-center justify-start gap-4 lg:w-[31%] md:w-[48%] max-md:w-full py-2 px-4 bg-transparent">
       <FontAwesomeIcon color="#066cc9" icon={props.icon} />
       <p className="text-[#9B9B9B] text-base font-medium whitespace-nowrap">{props.text}</p>
     </div>
