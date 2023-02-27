73e15910b1fef961b98261701780d838eb899280
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-02-27 10:48:30 +0330

    Image is responsive

--- src/Components/Body/WhyEdumall/WhyEdumall.jsx
+++ src/Components/Body/WhyEdumall/WhyEdumall.jsx
@@ -23,8 +23,10 @@ function WhyEdumall() {
         {IconTextData.map((data, index) => {
           return <IconTextComponent icon={data.icon} text={data.text} key={index} />;
         })}
-        <div className={`${styles.landingImage} w-full max-w-[84.375rem] h-[11.5625rem] rounded-lg relative mt-10`}>
-        <h3>قالب وردپرس آموزشی <span className="text-primary">همه کاره</span></h3>
+        <div className={`${styles.landingImage} w-full max-w-[84.375rem] h-[11.5625rem] rounded-lg flex items-center justify-start md:pr-4 lg:pr-8 mt-10`}>
+          <h3 className="text-2xl font-bold max-md:hidden">
+            قالب وردپرس آموزشی <span className="text-primary">همه کاره</span>
+          </h3>
         </div>
       </div>
     </section>
