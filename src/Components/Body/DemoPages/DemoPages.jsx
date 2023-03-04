b178a0c2c683ef5406089c625c34a90942909328
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-04 09:29:40 +0330

    line_after class is reusable

--- src/Components/Body/DemoPages/DemoPages.jsx
+++ src/Components/Body/DemoPages/DemoPages.jsx
@@ -4,7 +4,7 @@ function DemoPages() {
   return (
     <div className="py-28 bg-[#F5F7FD] w-full px-4 flex-center flex-col">
       <div className="flex-center gap-4">
-        <span className="max-md:text-5xl md:text-7xl font-bold text-primary">+۱۱</span>
+        <span className="max-md:text-5xl line_after md:text-7xl font-bold text-primary">+۱۱</span>
         <div className="flex justify-start items-center flex-col gap-2">
           <h3 className="max-md:text-2xl md:text-4xl font-medium">دموی</h3>
           <h3 className="max-md:text-2xl md:text-4xl font-medium">خانگی</h3>
