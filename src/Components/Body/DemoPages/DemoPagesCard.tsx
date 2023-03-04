c3e8e2251f9a95cbe6896cc46173071fe137b205
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-04 10:02:17 +0330

    Added A cricle

--- src/Components/Body/DemoPages/DemoPagesCard.tsx
+++ src/Components/Body/DemoPages/DemoPagesCard.tsx
@@ -4,7 +4,7 @@ import styles from "./cards.module.css"
 function DemoPagesCard() {
   return (
     <figure className={`${styles.wrapper} max-md:w-full md:w-[47%] lg:w-[32%] hover:bg-primary transition-all duration-300 cursor-pointer relative`}>
-        <div className='w-[13.75rem] h-[13.75rem] border border-white opacity-40 circle'></div>
+        <div className='w-[13.75rem] h-[13.75rem] border border-white opacity-40 circle translate-x-[50%] -translate-y-[50%] '></div>
 
     </figure>
   )
