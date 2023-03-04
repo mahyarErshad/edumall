9ed7fd5235e96b0f96dc9e4caaf4a4181410391a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-04 09:18:43 +0330

    Changed the text

--- src/Components/Body/DemoPages/DemoPages.jsx
+++ src/Components/Body/DemoPages/DemoPages.jsx
@@ -6,7 +6,7 @@ function DemoPages() {
       <div className="flex-center gap-4">
         <span className="max-md:text-5xl md:text-7xl font-bold text-primary">+۱۱</span>
         <div className="flex justify-start items-center flex-col gap-2">
-          <h3 className="max-md:text-2xl md:text-4xl font-medium">دموهای</h3>
+          <h3 className="max-md:text-2xl md:text-4xl font-medium">دموی</h3>
           <h3 className="max-md:text-2xl md:text-4xl font-medium">خانگی</h3>
         </div>
       </div>
