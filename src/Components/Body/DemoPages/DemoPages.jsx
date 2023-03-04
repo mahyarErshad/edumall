c38ba5fc43cc4ddad8d2e2685a2ff4fe31f36c31
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-04 09:04:30 +0330

    Added heading

--- src/Components/Body/DemoPages/DemoPages.jsx
+++ src/Components/Body/DemoPages/DemoPages.jsx
@@ -4,10 +4,11 @@ function DemoPages() {
   return (
     <div className="py-28 bg-[#F5F7FD] w-full px-4 flex-center flex-col">
       <div className="flex-center gap-4">
-        <span>+۱۱</span>
-        <h3>
-          دموهای <br /> خانگی
-        </h3>
+        <span className="max-md:text-5xl md:text-7xl font-bold text-primary">+۱۱</span>
+        <div className="flex justify-start items-center flex-col gap-2">
+          <h3 className="max-md:text-2xl md:text-4xl font-medium">دموهای</h3>
+          <h3 className="max-md:text-2xl md:text-4xl font-medium">خانگی</h3>
+        </div>
       </div>
     </div>
   );
