e5623aeb76dbb466d376049b72a805ae1fe34bb8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-04 09:31:21 +0330

    Texts are bigger

--- src/Components/Body/DemoPages/DemoPages.jsx
+++ src/Components/Body/DemoPages/DemoPages.jsx
@@ -4,10 +4,10 @@ function DemoPages() {
   return (
     <div className="py-28 bg-[#F5F7FD] w-full px-4 flex-center flex-col">
       <div className="flex-center gap-4">
-        <span className="max-md:text-5xl line_after md:text-7xl font-bold text-primary">+۱۱</span>
-        <div className="flex justify-start items-center flex-col gap-2">
-          <h3 className="max-md:text-2xl md:text-4xl font-medium">دموی</h3>
-          <h3 className="max-md:text-2xl md:text-4xl font-medium">خانگی</h3>
+        <span className="max-md:text-5xl line_after md:text-8xl font-bold text-primary">+۱۱</span>
+        <div className="flex justify-start items-center flex-col gap-3">
+          <h3 className="max-md:text-2xl md:text-5xl font-medium">دموی</h3>
+          <h3 className="max-md:text-2xl md:text-5xl font-medium">خانگی</h3>
         </div>
       </div>
     </div>
