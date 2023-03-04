792697e8a7fad7f6e5130822661d403eb12dca75
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-04 08:57:45 +0330

    Added text section

--- src/Components/Body/DemoPages/DemoPages.jsx
+++ src/Components/Body/DemoPages/DemoPages.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function DemoPages() {
   return (
-    <div>DemoPages</div>
-  )
+    <div className="py-28 bg-[#F5F7FD] w-full px-4 flex-center flex-col">
+      <div className="flex-center gap-4">
+        <span>+۱۱</span>
+        <h3>
+          دموهای <br /> خانگی
+        </h3>
+      </div>
+    </div>
+  );
 }
 
-export default DemoPages
+export default DemoPages;
