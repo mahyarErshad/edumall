8c8ce33904b09d7eb02312318cf0dab2d84431db
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-06 10:26:52 +0330

    TutorSection is created

---
+++ src/Components/Body/TutorSection/TutorSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TutorSection() {
+  return (
+   <section className='pt-32 bg-white px-4 flex-center flex-col'>
+
+   </section>
+  )
+}
+
+export default TutorSection
