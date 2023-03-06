cab0a15faa3171682033a03a9b6fca3c0f30e016
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-06 09:08:08 +0330

    Image is added

--- src/Components/Body/DemoPages/DemoPagesCard.tsx
+++ src/Components/Body/DemoPages/DemoPagesCard.tsx
@@ -7,12 +7,13 @@ function DemoPagesCard() {
       <div className={styles.imageSection}>
         <div className={`${styles.shapes} absolute w-full h-full top-0 transition-all`}>
           <div className="w-[13.75rem] h-[13.75rem] border border-white opacity-40 circle translate-x-[50%] -translate-y-[50%] absolute"></div>
-          <div className="w-[12.75rem] h-[12.75rem] border border-white bottom-0 left-0 opacity-40 circle -translate-x-[50%] translate-y-[50%] absolute"></div>
-          <div className="w-[20.625rem] h-[20.625rem] border border-white bottom-0 left-0 opacity-40 circle -translate-x-[50%] translate-y-[50%] absolute"></div>
-          <div className="w-[1.75rem] h-[1.75rem] top-[40%] left-0 circle -translate-x-[50%] absolute bg-gradient-to-b from-[#fad961] to-[#f76b1c]"></div>
-          <div className="w-[2.3125rem] h-[2.3125rem] top-[60%] right-0 circle translate-x-[40%] absolute bg-gradient-to-b from-[#b4ec51] to-[#429321]"></div>
-          <div className="w-[0.875rem] h-[0.875rem] top-[50%] right-[10%] circle translate-x-[40%] absolute bg-gradient-to-b from-[#fff4ce] to-[#ffc221]"></div>
+          <div className="w-[12.75rem] h-[12.75rem] border border-white bottom-0 left-0 opacity-40 circle -translate-x-[50%] absolute"></div>
+          <div className="w-[20.625rem] h-[20.625rem] border border-white bottom-0 left-0 opacity-40 circle -translate-x-[50%] translate-y-[25%] absolute"></div>
+          <div className="w-[1.75rem] h-[1.75rem] top-[25%] left-0 circle -translate-x-[50%] absolute bg-gradient-to-b from-[#fad961] to-[#f76b1c]"></div>
+          <div className="w-[2.3125rem] h-[2.3125rem] top-[55%] right-0 circle translate-x-[40%] absolute bg-gradient-to-b from-[#b4ec51] to-[#429321]"></div>
+          <div className="w-[0.875rem] h-[0.875rem] top-[45%] right-[10%] circle translate-x-[40%] absolute bg-gradient-to-b from-[#fff4ce] to-[#ffc221]"></div>
         </div>
+        <img className={styles.image} src="https://edumall.thememove.com/wp-content/uploads/2020/11/home-main-preview.jpg" alt="" />
       </div>
       <div className={styles.titleSection}></div>
     </figure>
