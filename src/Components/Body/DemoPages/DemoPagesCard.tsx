e9fb92f0a05acd45ad7208b105601b28ed47b4f4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-06 08:57:05 +0330

    Worked on card's UI

--- src/Components/Body/DemoPages/DemoPagesCard.tsx
+++ src/Components/Body/DemoPages/DemoPagesCard.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import styles from "./cards.module.css"
+import React from "react";
+import styles from "./cards.module.css";
 
 function DemoPagesCard() {
   return (
     <figure className={`${styles.wrapper} max-md:w-full md:w-[47%] lg:w-[32%] hover:bg-primary transition-all duration-300 cursor-pointer relative`}>
-        <div className='w-[13.75rem] h-[13.75rem] border border-white opacity-40 circle translate-x-[50%] -translate-y-[50%] absolute'></div>
-
+      <div className={styles.imageSection}>
+        <div className={`${styles.shapes} absolute w-full h-full top-0 transition-all`}>
+          <div className="w-[13.75rem] h-[13.75rem] border border-white opacity-40 circle translate-x-[50%] -translate-y-[50%] absolute"></div>
+          <div className="w-[12.75rem] h-[12.75rem] border border-white bottom-0 left-0 opacity-40 circle -translate-x-[50%] translate-y-[50%] absolute"></div>
+          <div className="w-[20.625rem] h-[20.625rem] border border-white bottom-0 left-0 opacity-40 circle -translate-x-[50%] translate-y-[50%] absolute"></div>
+          <div className="w-[1.75rem] h-[1.75rem] top-[40%] left-0 circle -translate-x-[50%] absolute bg-gradient-to-b from-[#fad961] to-[#f76b1c]"></div>
+          <div className="w-[2.3125rem] h-[2.3125rem] top-[60%] right-0 circle translate-x-[40%] absolute bg-gradient-to-b from-[#b4ec51] to-[#429321]"></div>
+          <div className="w-[0.875rem] h-[0.875rem] top-[50%] right-[10%] circle translate-x-[40%] absolute bg-gradient-to-b from-[#fff4ce] to-[#ffc221]"></div>
+        </div>
+      </div>
+      <div className={styles.titleSection}></div>
     </figure>
-  )
+  );
 }
 
-export default DemoPagesCard
+export default DemoPagesCard;
