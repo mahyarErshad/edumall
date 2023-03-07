e56e5accc88a9ac0bafa14e251b1843dd369858b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-07 09:43:38 +0330

    Cards are created using grid

--- src/Components/Body/TutorSection/TutorSectionCards.tsx
+++ src/Components/Body/TutorSection/TutorSectionCards.tsx
@@ -9,7 +9,7 @@ interface IProps {
 
 function TutorSectionCards(props: IProps) {
   return (
-    <figure className="bg-transparent lg:w-[47%] max-lg:w-full">
+    <figure className={`${styles.grid} bg-transparent lg:w-[47%] max-lg:w-full`}>
       <img className="w-12" src={props.image} alt={props.title} />
       <h3 className="text-2xl font-medium"> {props.title} </h3>
       <p className="font-normal text-sm"> {props.description} </p>
