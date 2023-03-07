74be09d70cbfe94c244895c2183268f0cd449bab
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-07 14:07:53 +0330

    Image has imported

--- src/Components/Body/ZoomVideo/ZoomVideo.jsx
+++ src/Components/Body/ZoomVideo/ZoomVideo.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import image from "../../../assets/images/zoom-meeting-image.png"
 
 function ZoomVideo() {
   return <section className="px-4 w-full pt-40 pb-24 bg-white flex flex-wrap">
-    <div className="max-md:w-full md:w-2/4"></div>
+    <div className="max-md:w-full md:w-2/4 flex-center relative">
+
+    </div>
     <div className="max-md:w-full md:w-2/4"></div>
   </section>;
 }
