ad009adcfd4e93a835748f50eb55fa85a4a70c35
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-07 10:21:22 +0330

    Changed the alt text

--- src/Components/Body/TutorSection/TutorMouseHoverAnimation.jsx
+++ src/Components/Body/TutorSection/TutorMouseHoverAnimation.jsx
@@ -6,9 +6,9 @@ import tutorLogo from "../../../assets/images/tutor-hover-animations/tutor-logo.
 function TutorMouseHoverAnimation() {
   return (
     <div className="max-lg:hidden lg:w-2/4">
-      <MouseParallaxContainer containerStyle={{overflow:"visible"}} resetOnLeave={true} useWindowMouseEvents={true} globalFactorX={0.1} globalFactorY={0.1} className="absolute flex-center bg-transparent w-full min-h-[38rem]">
+      <MouseParallaxContainer containerStyle={{ overflow: "visible" }} resetOnLeave={true} useWindowMouseEvents={true} globalFactorX={0.1} globalFactorY={0.1} className="absolute flex-center bg-transparent w-full min-h-[38rem]">
         <MouseParallaxChild factorX={0} factorY={0}>
-          <img src={main} alt="main image of tutor section" />
+          <img src={main} alt="tutor demo" />
         </MouseParallaxChild>
         <MouseParallaxChild className="absolute top-[5%] right-[5%]">
           <img src={tutorLogo} alt="tutor logo" className="rounded-[27px] shadow-lg" />
