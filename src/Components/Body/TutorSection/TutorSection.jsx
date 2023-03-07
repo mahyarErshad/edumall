4fd8c098f271fa8359d5af9546c04b4eb1cd4584
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-07 09:59:33 +0330

    Changed responsiveness

--- src/Components/Body/TutorSection/TutorSection.jsx
+++ src/Components/Body/TutorSection/TutorSection.jsx
@@ -17,7 +17,7 @@ function TutorSection() {
         <p>اجومال با قدرتمندترین پلاگین LMS - تیوتر LMS ارائه می شود که برای مربیان فردی، سازمان های آموزشی و پلت فرم آموزش الکترونیکی تخصص دارد.</p>
       </div>
       <div className="flex justify-between items-center flex-wrap mt-9">
-        <div className="max-md:w-full md:w-2/4 flex gap-4 flex-wrap">
+        <div className="max-lg:w-full lg:w-2/4 flex gap-4 flex-wrap">
           <TutorSectionCards image={arrow} title="سازنده دوره بصری" description="کاربران به راحتی می توانند دوره ها را با کشیدن و رها کردن سیستم و رابط کاربر پسند ایجاد و مدیریت کنند." />
           <TutorSectionCards image={eLearning} title="مدیریت آموزش الکترونیکی ساده" description="شما کنترل کامل دوره، مدرسان، سیستم جوایز و هر چیزی که به پلتفرم آموزش الکترونیکی شما مربوط می شود، دارید." />
           <TutorSectionCards image={reports} title="گزارش ها و تحلیل ها" description="همه کاربران آمار خود را به طور خودکار تولید می کنند تا پیشرفت و گزارش ها را پیگیری کنند" />
@@ -25,7 +25,7 @@ function TutorSection() {
           <TutorSectionCards image={money} title="درآمد و کمیسیون" description="با تیوتر LMS، می توانید درصد کمیسیون را تعیین کنید، کمیسیون را توزیع کنید و تمام فرآیندها را مدیریت کنید." />
           <TutorSectionCards image={awards} title="اعطای گواهینامه" description="تیوتر LMS گواهینامه های حرفه ای از پیش طراحی شده را به دانش آموزان ارائه می دهد یا می توانید گواهی سفارشی خود را ایجاد کنید." />
         </div>
-        <div className="max-md:w-full md:w-2/4"></div>
+        <div className="max-lg:hidden lg:w-2/4"></div>
       </div>
     </section>
   );
