991afbcdc65b1602eb996548dd785458bc290467
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-07 10:12:34 +0330

    Main image is added

--- src/Components/Body/TutorSection/TutorMouseHoverAnimation.jsx
+++ src/Components/Body/TutorSection/TutorMouseHoverAnimation.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { MouseParallaxChild, MouseParallaxContainer } from "react-parallax-mouse";
+import main from "../../../assets/images/tutor-hover-animations/main.jpg";
 
 function TutorMouseHoverAnimation() {
   return (
     <div className="max-lg:hidden lg:w-2/4">
-      <MouseParallaxContainer resetOnLeave={true} useWindowMouseEvents={true} globalFactorX={0.1} globalFactorY={0.1} className="absolute bg-transparent overflow-hidden w-full min-h-[38rem]">
-        <MouseParallaxChild className="absolute bottom-[-25rem] right-[-25rem] bg-[#0666BD] circle w-[50rem] h-[50rem]" >
-            
+      <MouseParallaxContainer resetOnLeave={true} useWindowMouseEvents={true} globalFactorX={0.1} globalFactorY={0.1} className="absolute flex-center bg-transparent overflow-hidden w-full min-h-[38rem]">
+        <MouseParallaxChild factorX={0} factorY={0} className="">
+          <img src={main} alt="main image of tutor section" />
         </MouseParallaxChild>
       </MouseParallaxContainer>
     </div>
