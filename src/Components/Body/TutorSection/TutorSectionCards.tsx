b3139cff9b261f36a25aaab4fc13412137380d08
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-07 10:00:07 +0330

    Cards are more responsive

--- src/Components/Body/TutorSection/TutorSectionCards.tsx
+++ src/Components/Body/TutorSection/TutorSectionCards.tsx
@@ -9,7 +9,7 @@ interface IProps {
 
 function TutorSectionCards(props: IProps) {
   return (
-    <figure className={`${styles.grid} bg-transparent lg:w-[47%] max-lg:w-full`}>
+    <figure className={`${styles.grid} bg-transparent md:w-[47%] max-md:w-full`}>
       <img className="w-12" src={props.image} alt={props.title} />
       <h3 className="text-2xl font-medium"> {props.title} </h3>
       <p className="font-normal text-sm"> {props.description} </p>
