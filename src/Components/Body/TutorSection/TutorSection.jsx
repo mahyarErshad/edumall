c533b31f1cb832f0bf6e652c4a751810161cbc2c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-07 08:30:25 +0330

    Added title text

--- src/Components/Body/TutorSection/TutorSection.jsx
+++ src/Components/Body/TutorSection/TutorSection.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function TutorSection() {
   return (
-   <section className='pt-32 bg-white px-4 flex-center flex-col'>
-
-   </section>
-  )
+    <section className="pt-32 bg-white px-4 flex-center flex-col">
+      <div className="max-md:w-full md:w-2/4 bg-transparent flex flex-col gap-8 max-md:text-center">
+        <h2 className="font-medium max-md:text-3xl md:text-5xl leading-5">
+          ادغام با افزونه <span className="line_after">تیوتر</span> LMS
+        </h2>
+        <p>اجومال با قدرتمندترین پلاگین LMS - تیوتر LMS ارائه می شود که برای مربیان فردی، سازمان های آموزشی و پلت فرم آموزش الکترونیکی تخصص دارد.</p>
+      </div>
+    </section>
+  );
 }
 
-export default TutorSection
+export default TutorSection;
