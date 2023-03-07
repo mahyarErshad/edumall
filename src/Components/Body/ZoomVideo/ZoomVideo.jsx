1f1cb3640508c9f080d78decddbf0805254785b6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-07 14:46:07 +0330

    Added second circle

--- src/Components/Body/ZoomVideo/ZoomVideo.jsx
+++ src/Components/Body/ZoomVideo/ZoomVideo.jsx
@@ -10,10 +10,11 @@ function ZoomVideo() {
         <div className="absolute top-0 left-0 flex-center -translate-y-[75%]">
           <MouseParallaxContainer containerStyle={{ overflow: "visible" }} className="relative" resetOnLeave={true} useWindowMouseEvents={true} globalFactorX={0.02} globalFactorY={0.02}>
             <MouseParallaxChild className="absolute h-28 w-28 border border-[#EFF1FF] circle bg-transparent top-[-3.625rem]" />
+            <MouseParallaxChild className="absolute h-56 w-56 border border-[#EFF1FF] circle bg-transparent top-[-7.1875rem] left-[-10.9375rem]" />
           </MouseParallaxContainer>
           <img src={logo} alt="Logo" />
         </div>
-        <img src={image} alt="Zoom meet video" />
+        <img src={image} alt="Zoom meet video" className="z-10" />
       </div>
       <div className="max-md:w-full md:w-2/4"></div>
     </section>
