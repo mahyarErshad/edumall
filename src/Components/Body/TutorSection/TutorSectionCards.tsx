8a5fc4902ba495a497d31fea9257afc18182c10e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-07 09:39:59 +0330

    Created module css file

--- src/Components/Body/TutorSection/TutorSectionCards.tsx
+++ src/Components/Body/TutorSection/TutorSectionCards.tsx
@@ -8,7 +8,7 @@ interface IProps {
 
 function TutorSectionCards(props: IProps) {
   return (
-    <figure className="bg-transparent flex flex-col gap-4 justify-start lg:w-[47%] max-lg:w-full">
+    <figure className="bg-transparent lg:w-[47%] max-lg:w-full">
       <img className="w-12" src={props.image} alt={props.title} />
       <h3 className="text-2xl font-medium"> {props.title} </h3>
       <p className="font-normal text-sm"> {props.description} </p>
