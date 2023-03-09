f75be2cdb185c14ec288e6fd95f5de6d27fc9cc2
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-09 08:38:22 +0330

    h3 is added

--- src/Components/Body/CourseManagementSystem/CourseManagementSystem.jsx
+++ src/Components/Body/CourseManagementSystem/CourseManagementSystem.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function CourseManagementSystem() {
-  return <section className="w-full px-4 bg-[#F5F7FD] py-24">CourseManagementSystem</section>;
+  return (
+    <section className="w-full px-4 bg-[#F5F7FD] py-24 flex justify-start items-center flex-col">
+      <h3>سیستم مدیریت دوره</h3>
+    </section>
+  );
 }
 
 export default CourseManagementSystem;
