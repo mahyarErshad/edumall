b0ff94751eef3693b9ca5e9a31fc36521a4f6425
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-09 08:36:18 +0330

    Container is added

--- src/Components/Body/CourseManagementSystem/CourseManagementSystem.jsx
+++ src/Components/Body/CourseManagementSystem/CourseManagementSystem.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function CourseManagementSystem() {
-  return (
-    <div>CourseManagementSystem</div>
-  )
+  return <section className="w-full px-4 bg-[#F5F7FD] py-24">CourseManagementSystem</section>;
 }
 
-export default CourseManagementSystem
+export default CourseManagementSystem;
