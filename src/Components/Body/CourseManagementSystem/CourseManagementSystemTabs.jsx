722195499ea121a4042729403ba0eb5863572e8b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-09 08:55:01 +0330

    Created module css file

--- src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
+++ src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 function CourseManagementSystemTabs() {
   return (
     <section className="w-full flex-center flex-wrap">
+    <div className='w-[55%]'>
+
+    </div>
+    <div className='w-[45%]'>
+        
+    </div>
       
     </section>
   )
