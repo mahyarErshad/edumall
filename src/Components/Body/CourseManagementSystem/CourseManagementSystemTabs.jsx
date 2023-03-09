b5c7419635e0daa2fda55a49a83f70ff889e1729
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-09 12:12:16 +0330

    Image has it's styles

--- src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
+++ src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
@@ -4,7 +4,7 @@ function CourseManagementSystemTabs() {
   return (
     <section className="w-full flex-center flex-wrap">
       <div className="w-[55%] flex-center relative">
-        <img src="https://edumall.thememove.com/wp-content/uploads/2020/11/course-addons-01.jpg" alt="" />
+        <img className="border-[0.375rem] border-[#333] shadow-lg rounded-[1.25rem]" src="https://edumall.thememove.com/wp-content/uploads/2020/11/course-addons-01.jpg" alt="" />
       </div>
       <div className="w-[45%]"></div>
     </section>
