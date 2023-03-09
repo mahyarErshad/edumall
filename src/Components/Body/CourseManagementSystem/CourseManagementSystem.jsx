0523d1cd8e26cd299728fd3ba96ed25b86490bbc
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-09 08:40:12 +0330

    Heading is styled

--- src/Components/Body/CourseManagementSystem/CourseManagementSystem.jsx
+++ src/Components/Body/CourseManagementSystem/CourseManagementSystem.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function CourseManagementSystem() {
   return (
     <section className="w-full px-4 bg-[#F5F7FD] py-24 flex justify-start items-center flex-col">
-      <h3>سیستم مدیریت دوره</h3>
+      <h3 className="max-md:text-3xl md:text-5xl font-medium">
+        سیستم <span className="line_after">مدیریت</span> دوره
+      </h3>
     </section>
   );
 }
