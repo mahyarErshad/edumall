dc408d0903b48d9c12245ab3d6ab6a6acca9acae
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-09 12:09:53 +0330

    Removed unnecessary code and files

--- src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
+++ src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import styles from "./tabs.module.css";
 
 function CourseManagementSystemTabs() {
   return (
     <section className="w-full flex-center flex-wrap">
-      <div className={`w-[55%] ${styles.imageTab}`}>
+      <div className="w-[55%] flex-center relative">
         <img src="https://edumall.thememove.com/wp-content/uploads/2020/11/course-addons-01.jpg" alt="" />
       </div>
       <div className="w-[45%]"></div>
