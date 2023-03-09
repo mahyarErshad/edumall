e49b12465ef351c4f740f375018dccbe6d803ddb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-09 12:28:03 +0330

    Removed h-96 class

--- src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
+++ src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
@@ -6,7 +6,7 @@ function CourseManagementSystemTabs() {
       <div className="w-[55%] flex-center">
         <img className="border-[0.375rem] border-[#333] test shadow-lg rounded-[1.25rem] h-full z-10" src="https://edumall.thememove.com/wp-content/uploads/2020/11/course-addons-01.jpg" alt="" />
       </div>
-      <div className="w-[45%] h-96"></div>
+      <div className="w-[45%]"></div>
     </section>
   );
 }
