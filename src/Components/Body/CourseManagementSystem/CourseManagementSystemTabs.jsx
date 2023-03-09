93af4cb366cfce12877fcb0a885316c168904094
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-09 10:15:36 +0330

    Tabs shape has fill="#F0F3FB"

--- src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
+++ src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import styles from "./tabs.module.css"
+import React from "react";
+import styles from "./tabs.module.css";
 
 function CourseManagementSystemTabs() {
   return (
     <section className="w-full flex-center flex-wrap">
-    <div className={`w-[55%] ${styles.imageTab}`}>
-
-    </div>
-    <div className='w-[45%]'>
-
-    </div>
-      
+      <div className={`w-[55%] ${styles.imageTab}`}>
+        <img src="https://edumall.thememove.com/wp-content/uploads/2020/11/course-addons-01.jpg" alt="" />
+      </div>
+      <div className="w-[45%]"></div>
     </section>
-  )
+  );
 }
 
-export default CourseManagementSystemTabs
+export default CourseManagementSystemTabs;
