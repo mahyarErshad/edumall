cf45108dda6cd086f5823671dea436dc728f1eae
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-09 12:29:00 +0330

    Width is 50-50

--- src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
+++ src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function CourseManagementSystemTabs() {
   return (
     <section className="w-full flex-center flex-wrap mt-10">
-      <div className="w-[55%] flex-center">
+      <div className="w-[50%] flex-center">
         <img className="border-[0.375rem] border-[#333] test shadow-lg rounded-[1.25rem] h-full z-10" src="https://edumall.thememove.com/wp-content/uploads/2020/11/course-addons-01.jpg" alt="" />
       </div>
-      <div className="w-[45%]"></div>
+      <div className="w-[50%]"></div>
     </section>
   );
 }
