b1604379ae3d0af693c6bbe9b7fdf8b51359dafe
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-09 08:46:50 +0330

    Texts are added and styled

--- src/Components/Body/CourseManagementSystem/CourseManagementSystem.jsx
+++ src/Components/Body/CourseManagementSystem/CourseManagementSystem.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 function CourseManagementSystem() {
   return (
-    <section className="w-full px-4 bg-[#F5F7FD] py-24 flex justify-start items-center flex-col">
+    <section className="w-full px-4 bg-[#F5F7FD] py-24 flex justify-start items-center flex-col gap-8">
       <h3 className="max-md:text-3xl md:text-5xl font-medium">
         سیستم <span className="line_after">مدیریت</span> دوره
       </h3>
+      <p className="text-center text-[#7E7E7E]">اجومال با پلاگین تیوتر LMS یکپارچه شده است و دارای یک سیستم کامل برای مدیریت دوره های آموزشی است.</p>
+      <div className="w-full flex-center flex-wrap">
+      
+      </div>
     </section>
   );
 }
