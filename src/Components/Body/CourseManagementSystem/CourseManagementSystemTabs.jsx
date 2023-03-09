cd4602a0313a8d46c10fe2c7209403d5240b8173
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-09 12:13:06 +0330

    Margin top is 2.5 rem

--- src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
+++ src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function CourseManagementSystemTabs() {
   return (
-    <section className="w-full flex-center flex-wrap">
+    <section className="w-full flex-center flex-wrap mt-10">
       <div className="w-[55%] flex-center relative">
         <img className="border-[0.375rem] border-[#333] shadow-lg rounded-[1.25rem]" src="https://edumall.thememove.com/wp-content/uploads/2020/11/course-addons-01.jpg" alt="" />
       </div>
