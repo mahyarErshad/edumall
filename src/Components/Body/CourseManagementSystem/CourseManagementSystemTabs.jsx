50f83e25dbfbc30d6fe8de75954d71d8d7795a5a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-11 11:31:08 +0330

    Another Tab is created

--- src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
+++ src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
@@ -22,6 +22,11 @@ function CourseManagementSystemTabs() {
             <h3 className="text-black text-xl font-medium">تنظیمات آزمون پیشرفته</h3>
             <p className={`${styles.description} text-sm font-normal text-[#696969]`}>تنظیمات مسابقه به شما امکان می‌دهد انواع سؤالات را انتخاب کنید، محدودیت‌های زمانی تعیین کنید و بسیاری موارد دیگر.</p>
           </Tab>
+          <Tab className={`gap-4 w-full py-6 px-8 rounded-lg cursor-pointer ${styles.tab} tab`}>
+            <QuestionMark className="w-8 h-8 transition-all duration-300" />
+            <h3 className="text-black text-xl font-medium">چندین مربی</h3>
+            <p className={`${styles.description} text-sm font-normal text-[#696969]`}>یک دوره می تواند بیش از یک مدرس داشته باشد. شما می توانید هر تعداد مربی که می خواهید داشته باشید.</p>
+          </Tab>
         </TabList>
       </div>
       <div className="w-[65%] flex-center">
