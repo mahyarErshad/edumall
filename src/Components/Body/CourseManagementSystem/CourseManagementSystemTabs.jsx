dfadcde1143d9ed1955cba5e48bbdb8923af8f7c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-11 09:47:09 +0330

    Changed the width

--- src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
+++ src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
@@ -7,7 +7,7 @@ import styles from "./CourseManagementSystemTabs.module.css";
 function CourseManagementSystemTabs() {
   return (
     <Tabs className="w-full flex-center flex-wrap gap-[5%] mt-10">
-      <div className="w-[35%]">
+      <div className="w-[30%]">
         <TabList className="flex flex-col justify-start h-full gap-3">
           <Tab className={`flex justify-start gap-8 w-full py-6 px-8 rounded-lg ${styles.tab}`}>
             <FontAwesomeIcon className="w-8 h-8" icon={faBacon} />
@@ -17,7 +17,7 @@ function CourseManagementSystemTabs() {
           <Tab>Title 2</Tab>
         </TabList>
       </div>
-      <div className="w-[60%] flex-center">
+      <div className="w-[65%] flex-center">
         <TabPanel>
           <img className="border-[0.375rem] border-[#333] test shadow-lg rounded-[1.25rem] h-full z-10" src="https://edumall.thememove.com/wp-content/uploads/2020/11/course-addons-01.jpg" alt="" />
         </TabPanel>
