3d1aa8faec68b0b4d3df40cd506c35078d1b3a8b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-11 13:18:18 +0330

    Removed tabPanel class

--- src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
+++ src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
@@ -38,7 +38,7 @@ function CourseManagementSystemTabs() {
           </Tab>
         </TabList>
       </div>
-      <div className={`${styles.tabPanel} max-md:w-full md:w-[65%] flex-center`}>
+      <div className="max-md:w-full md:w-[65%] flex-center">
         <TabPanel>
           <img className="border-[0.375rem] border-[#333] test shadow-lg rounded-[1.25rem] h-full z-10" src={courseAddons} alt="course-addons" />
         </TabPanel>
