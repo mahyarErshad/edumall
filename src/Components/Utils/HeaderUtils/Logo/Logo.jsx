55347bccafc1747206739c5badd0d83eb6caae79
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-11 16:54:15 +0330

    Fixed scrollTo issue

--- src/Components/Utils/HeaderUtils/Logo/Logo.jsx
+++ src/Components/Utils/HeaderUtils/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import darkLogo from "../../../../assets/images/dark-logo.png";
 
 function Logo({ dark }) {
   return (
-    <div onClick={() => window.scrollTo({ top: 0 })} className="flex-center h-full cursor-pointer">
+    <div onClick={() => document.getElementById("container").scrollTo({ top: 0 })} className="flex-center h-full cursor-pointer">
       <img className="max-w-[9.375rem]" src={dark ? darkLogo : logo} alt="Logo" />
     </div>
   );
