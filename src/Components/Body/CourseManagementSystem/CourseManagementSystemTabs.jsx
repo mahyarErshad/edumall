2880bf6cd11bfb0e0ff7913c87e3829cb6f612d5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-11 09:42:18 +0330

    Elements of each tab is created

--- src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
+++ src/Components/Body/CourseManagementSystem/CourseManagementSystemTabs.jsx
@@ -1,3 +1,5 @@
+import { faBacon } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import styles from "./CourseManagementSystemTabs.module.css";
@@ -5,6 +7,16 @@ import styles from "./CourseManagementSystemTabs.module.css";
 function CourseManagementSystemTabs() {
   return (
     <Tabs className="w-full flex-center flex-wrap gap-[5%] mt-10">
+      <div className="w-[35%]">
+        <TabList className="flex flex-col justify-start h-full gap-3">
+          <Tab className={`flex justify-start gap-8 w-full py-6 px-8 rounded-lg ${styles.tab}`}>
+            <FontAwesomeIcon className="w-8 h-8" icon={faBacon} />
+            <h3 className="text-black text-xl font-medium">سازنده دوره مقدماتی</h3>
+            <p className="text-sm font-normal text-[#696969]">کاربران می‌توانند محتوای دوره و آزمون‌ها را ایجاد کنند یا مدرس را به راحتی اضافه کنند.</p>
+          </Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
+      </div>
       <div className="w-[60%] flex-center">
         <TabPanel>
           <img className="border-[0.375rem] border-[#333] test shadow-lg rounded-[1.25rem] h-full z-10" src="https://edumall.thememove.com/wp-content/uploads/2020/11/course-addons-01.jpg" alt="" />
@@ -12,12 +24,6 @@ function CourseManagementSystemTabs() {
         <TabPanel>
           <h2>Any content 2</h2>
         </TabPanel>
-      </div>
-      <div className="w-[35%]">
-        <TabList className="flex flex-col gap-3">
-          <Tab className={`flex justify-start gap-8 w-full py-6 px-8 rounded-lg ${styles.tab}`}></Tab>
-          <Tab>Title 2</Tab>
-        </TabList>
       </div>
     </Tabs>
   );
