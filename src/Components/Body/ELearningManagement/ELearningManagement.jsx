74600961d8cd14155a2c9b811c4d9fa7b1e4f895
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-12 13:15:19 +0330

    Cards are responsive

--- src/Components/Body/ELearningManagement/ELearningManagement.jsx
+++ src/Components/Body/ELearningManagement/ELearningManagement.jsx
@@ -6,22 +6,22 @@ import controlIcon from "../../../assets/images/ELearningManagement/control.svg"
 
 function ELearningManagement() {
   return (
-    <section className="w-full px-4 bg-[#F8F8F8] pt-32 pb-20 flex justify-between max-md:flex-col items-center overflow-hidden">
-      <MouseParallaxContainer resetOnLeave={true} useWindowMouseEvents={true} globalFactorX={0.1} globalFactorY={0.1} containerStyle={{ overflow: "visible" }} className="max-md:w-full md:w-[45%] flex-center flex-col gap-10 md:pr-[10%] relative">
+    <section className="w-full px-4 bg-[#F8F8F8] pt-32 pb-20 flex justify-between max-lg:flex-col items-center overflow-hidden gap-8">
+      <MouseParallaxContainer resetOnLeave={true} useWindowMouseEvents={true} globalFactorX={0.1} globalFactorY={0.1} containerStyle={{ overflow: "visible" }} className="max-lg:w-full lg:w-[45%] flex-center flex-col gap-10 md:pr-[10%] relative">
         <h3 className="font-medium max-md:text-2xl md:text-3xl lg:text-4xl">
           برای مدیریت آموزش الکترونیکی <span className="line_after">بهینه</span> شده
         </h3>
         <p className="text-[#696969]">تیوتر LMS کاربران را قادر می سازد تا پلتفرم آموزش الکترونیکی شما را به راحتی مدیریت کنند و بر تمام دوره های شما نظارت داشته باشند</p>
-        <MouseParallaxChild className="absolute -bottom-[100%] right-[10%]">
+        <MouseParallaxChild className="absolute -bottom-[100%] right-[10%] max-lg:hidden">
           <img src={wavyImage} alt="wavy circle shape" />
         </MouseParallaxChild>
       </MouseParallaxContainer>
-      <div className="max-md:w-full md:flex-grow flex-center flex-col gap-5">
-        <div className="flex-center gap-5 pl-[5.625rem]">
+      <div className="max-lg:w-full lg:flex-grow flex-center flex-col gap-5">
+        <div className="flex-center gap-5 lg:pl-[5.625rem] flex-wrap">
           <ELearningManagementCards image={controlIcon} title="ثبت نام دانشجویان را کنترل کنید" />
           <ELearningManagementCards image={controlIcon} title="ثبت نام دانشجویان را کنترل کنید" />
         </div>
-        <div className="flex-center gap-5 pr-[5.625rem]">
+        <div className="flex-center gap-5 lg:pr-[5.625rem] flex-wrap">
           <ELearningManagementCards image={controlIcon} title="ثبت نام دانشجویان را کنترل کنید" />
           <ELearningManagementCards image={controlIcon} title="ثبت نام دانشجویان را کنترل کنید" />
         </div>
--- src/Components/Body/ELearningManagement/ELearningManagementCards.tsx
+++ src/Components/Body/ELearningManagement/ELearningManagementCards.tsx
@@ -8,7 +8,7 @@ interface IProps {
 
 function ELearningManagementCards(props: IProps) {
   return (
-    <figure className={`${props.className} py-10 px-8 bg-white rounded-lg shadow-md flex-center gap-4 z-10 max-w-[21.25rem]`}>
+    <figure className={`${props.className} py-10 px-8 bg-white rounded-lg max-md:w-full md:w-[45%] shadow-md flex-center gap-4 z-10 max-w-[21.25rem]`}>
       <img src={props.image} alt={props.title} />
       <h3 className="font-medium text-xl">{props.title}</h3>
     </figure>
