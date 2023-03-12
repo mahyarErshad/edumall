c0c75444844107d3c8de67deea8c3324d9959aca
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-12 08:16:36 +0330

    Changed the p tag

--- src/Components/Body/AdministrationsOption/AdministrationsOption.jsx
+++ src/Components/Body/AdministrationsOption/AdministrationsOption.jsx
@@ -5,9 +5,9 @@ function AdministrationsOption() {
   return (
     <section className="w-full px-4 bg-white py-24 flex justify-start items-center flex-col gap-8 overflow-hidden">
       <h3 className="max-md:text-3xl md:text-5xl font-medium">
-        سیستم <span className="line_after">مدیریت</span> دوره
+        گزینه های <span className="line_after">مدیریت</span>
       </h3>
-      <p className="text-center text-[#7E7E7E]">اجومال با پلاگین تیوتر LMS یکپارچه شده است و دارای یک سیستم کامل برای مدیریت دوره های آموزشی است.</p>
+      <p className="text-center text-[#7E7E7E]">اجومال به لطف ادغام افزونه تیوتر LMS کنترل کامل وب سایت آموزشی شما را در اختیار کاربران قرار می دهد.</p>
       <CourseManagementSystemTabs />
     </section>
   );
