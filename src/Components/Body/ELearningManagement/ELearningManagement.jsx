4c2da263096ea2054ed6ce99930cf7dfbc5b98fa
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-12 10:43:11 +0330

    2 child elements are added

--- src/Components/Body/ELearningManagement/ELearningManagement.jsx
+++ src/Components/Body/ELearningManagement/ELearningManagement.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 function ELearningManagement() {
-  return <section className="w-full px-4 bg-[#F8F8F8] pt-32 pb-20">
-    
-  </section>;
+  return (
+    <section className="w-full px-4 bg-[#F8F8F8] pt-32 pb-20 flex justify-between items-center flex-wrap">
+      <div className="max-md:w-full md:w-[45%]"></div>
+      <div className="max-md:w-full md:w-[45%]"></div>
+    </section>
+  );
 }
 
 export default ELearningManagement;
