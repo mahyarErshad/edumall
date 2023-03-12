57bc0a2fff653d1402dd196318138cb96ceb8fb7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-12 09:42:19 +0330

    Section is styled

---
+++ src/Components/Body/ELearningManagement/ELearningManagement.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ELearningManagement() {
+  return <section className="w-full px-4 bg-[#F8F8F8] pt-32 pb-20">
+    
+  </section>;
+}
+
+export default ELearningManagement;
