df9a5d52dd77f644fd80b02d0e5a46dc9bf4a840
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-12 12:51:45 +0330

    Div become figure

--- src/Components/Body/ELearningManagement/ELearningManagementCards.jsx
+++ src/Components/Body/ELearningManagement/ELearningManagementCards.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function ELearningManagementCards() {
   return (
-    <div className="py-10 px-8 bg-white rounded-lg shadow-md flex-center gap-4 z-10">
+    <figure className="py-10 px-8 bg-white rounded-lg shadow-md flex-center gap-4 z-10">
       <img src={test} alt="" />
       <h3 className="font-medium text-xl">ثبت نام دانشجویان را کنترل کنید</h3>
-    </div>
+    </figure>
   );
 }
 
