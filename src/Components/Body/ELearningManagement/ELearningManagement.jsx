9ae0fff172739960ca5f8ce025bc301b1dc1ff54
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-12 11:18:49 +0330

    Shape has been added

--- src/Components/Body/ELearningManagement/ELearningManagement.jsx
+++ src/Components/Body/ELearningManagement/ELearningManagement.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { MouseParallaxChild, MouseParallaxContainer } from "react-parallax-mouse";
+import wavyImage from "../../../assets/images/wavy-shape.png";
 
 function ELearningManagement() {
   return (
-    <section className="w-full px-4 bg-[#F8F8F8] pt-32 pb-20 flex justify-between items-center flex-wrap">
-      <div className="max-md:w-full md:w-[45%] flex-center flex-col gap-10 md:pr-[10%]">
+    <section className="w-full px-4 bg-[#F8F8F8] pt-32 pb-20 flex justify-between items-center flex-wrap overflow-hidden">
+      <MouseParallaxContainer resetOnLeave={true} useWindowMouseEvents={true} globalFactorX={0.1} globalFactorY={0.1} containerStyle={{ overflow: "visible" }} className="max-md:w-full md:w-[45%] flex-center flex-col gap-10 md:pr-[10%] relative">
         <h3 className="font-medium max-md:text-2xl md:text-3xl lg:text-4xl">
           برای مدیریت آموزش الکترونیکی <span className="line_after">بهینه</span> شده
         </h3>
         <p className="text-[#696969]">تیوتر LMS کاربران را قادر می سازد تا پلتفرم آموزش الکترونیکی شما را به راحتی مدیریت کنند و بر تمام دوره های شما نظارت داشته باشند</p>
-      </div>
+        <MouseParallaxChild className="absolute -bottom-[100%] right-[10%]">
+          <img src={wavyImage} alt="wavy circle shape" />
+        </MouseParallaxChild>
+      </MouseParallaxContainer>
       <div className="max-md:w-full md:flex-grow"></div>
     </section>
   );
