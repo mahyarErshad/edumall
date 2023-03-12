1caa2a2f74e99b592fcfdef08e29a49bfa15f804
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-12 12:53:21 +0330

    Defined interface

--- src/Components/Body/ELearningManagement/ELearningManagementCards.tsx
+++ src/Components/Body/ELearningManagement/ELearningManagementCards.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
-function ELearningManagementCards(props) {
+interface IProps {
+  title: string;
+  image: string;
+}
+
+function ELearningManagementCards(props: IProps) {
   return (
     <figure className="py-10 px-8 bg-white rounded-lg shadow-md flex-center gap-4 z-10">
       <img src={props.image} alt={props.title} />
