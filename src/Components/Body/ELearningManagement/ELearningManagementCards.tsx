54e3cd9143c2dfb834b7ac8f48eb8d09e2a7de3e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-12 12:54:55 +0330

    Can pass className via props

--- src/Components/Body/ELearningManagement/ELearningManagementCards.tsx
+++ src/Components/Body/ELearningManagement/ELearningManagementCards.tsx
@@ -3,11 +3,12 @@ import React from "react";
 interface IProps {
   title: string;
   image: string;
+  className? : string;
 }
 
 function ELearningManagementCards(props: IProps) {
   return (
-    <figure className="py-10 px-8 bg-white rounded-lg shadow-md flex-center gap-4 z-10">
+    <figure className={`${props.className} py-10 px-8 bg-white rounded-lg shadow-md flex-center gap-4 z-10`}>
       <img src={props.image} alt={props.title} />
       <h3 className="font-medium text-xl">{props.title}</h3>
     </figure>
