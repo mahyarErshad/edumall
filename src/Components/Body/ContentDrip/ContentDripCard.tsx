19f5729231dedb8012158b2737e65e4b7810a9cf
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 09:11:20 +0330

    Cards have padding

--- src/Components/Body/ContentDrip/ContentDripCard.tsx
+++ src/Components/Body/ContentDrip/ContentDripCard.tsx
@@ -8,12 +8,12 @@ interface IProps {
 
 function ContentDripCard(props: IProps) {
   return (
-    <figure className="flex-center flex-col gap-3 max-md:w-full md:max-w-[15.625rem]">
+    <figure className="flex-center flex-col gap-3 max-md:w-full max-w-[15.625rem]">
       <div className="flex-center w-full">
         <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">{props.counter}</span>
         <span className={`flex-grow h-1 ${props.hasDots && "background-infinite-dots"} max-md:bg-repeat-y md:bg-repeat-x w-full`}></span>
       </div>
-      <p className="font-medium">{props.text}</p>
+      <p className="font-medium pl-7">{props.text}</p>
     </figure>
   );
 }
