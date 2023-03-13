2661fbf0184f3796b689b5a6747a1765039f26e3
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 09:08:04 +0330

    Having dots is a prop

--- src/Components/Body/ContentDrip/ContentDrip.jsx
+++ src/Components/Body/ContentDrip/ContentDrip.jsx
@@ -7,7 +7,7 @@ function ContentDrip() {
       <div className="max-md:w-full md:w-[60%] flex-center flex-col">
         <h4 className="font-medium max-md:text-3xl md:text-5xl mb-16">جریان محتوا</h4>
         <div className="w-full flex-center">
-          <ContentDripCard counter="۱" text="بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید." />
+          <ContentDripCard counter="۱" text="بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید." hasDots />
         </div>
       </div>
       <div className="max-md:w-full md:w-[35%]"></div>
--- src/Components/Body/ContentDrip/ContentDripCard.tsx
+++ src/Components/Body/ContentDrip/ContentDripCard.tsx
@@ -3,6 +3,7 @@ import React from "react";
 interface IProps {
   counter: string | number;
   text: string;
+  hasDots: boolean;
 }
 
 function ContentDripCard(props: IProps) {
@@ -10,7 +11,7 @@ function ContentDripCard(props: IProps) {
     <figure className="flex-center flex-col gap-3 max-md:w-full md:max-w-[15.625rem]">
       <div className="flex-center w-full">
         <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">{props.counter}</span>
-        <span className="flex-grow h-1 background-infinite-dots max-md:bg-repeat-y md:bg-repeat-x w-full"></span>
+        <span className={`flex-grow h-1 ${props.hasDots && "background-infinite-dots"} max-md:bg-repeat-y md:bg-repeat-x w-full`}></span>
       </div>
       <p className="font-medium">{props.text}</p>
     </figure>
