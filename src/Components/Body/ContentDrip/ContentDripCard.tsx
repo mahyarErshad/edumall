fa047bfd5d55a95f0f6311cdb26deafb31f95347
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 10:44:48 +0330

    Card is responsive

--- src/Components/Body/ContentDrip/ContentDripCard.tsx
+++ src/Components/Body/ContentDrip/ContentDripCard.tsx
@@ -14,7 +14,7 @@ function ContentDripCard(props: IProps) {
         <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">{props.counter}</span>
         <span className={`flex-grow h-1 ${props.hasDots && styles['background-infinite-dots']} max-md:mt-4 max-md:bg-repeat-y md:bg-repeat-x max-md:h-8 max-md:w-2 md:w-full`}></span>
       </div>
-      <p className="font-medium pl-7 max-md:self-start max-md:translate-y-2">{props.text}</p>
+      <p className="font-medium pl-7 max-md:self-start max-md:translate-y-2 min-h-[4.5rem] mt-2">{props.text}</p>
     </figure>
   );
 }
