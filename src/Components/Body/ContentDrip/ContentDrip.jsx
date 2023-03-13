3d7cad6b380c56c6d975791e96efac4bde156eb9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 08:55:37 +0330

    Card is created

--- src/Components/Body/ContentDrip/ContentDrip.jsx
+++ src/Components/Body/ContentDrip/ContentDrip.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function ContentDrip() {
   return (
     <section className="w-full px-4 flex-center gap-8 max-md:flex-col pt-32 pb-20">
-      <div className="max-md:w-full md:w-[60%] flex-center flex-col gap-5">
-        <h4 className="font-medium max-md:text-3xl md:text-5xl">جریان محتوا</h4>
+      <div className="max-md:w-full md:w-[60%] flex-center flex-col">
+        <h4 className="font-medium max-md:text-3xl md:text-5xl mb-5">جریان محتوا</h4>
         <div className="w-full flex-center">
-          <figure className="flex-center flex-col max-w-[15.625rem]">
+          <figure className="flex-center flex-col gap-4 max-w-[15.625rem]">
             <div className="flex-center w-full">
               <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">1</span>
               <span className="flex-grow h-1 background-infinite-dots w-full"></span>
