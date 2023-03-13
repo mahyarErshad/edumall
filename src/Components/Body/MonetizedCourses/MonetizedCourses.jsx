e96a138ca68e81c6c8c25eeac88cc3b07404a522
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 14:01:54 +0330

    Changed the heading

--- src/Components/Body/MonetizedCourses/MonetizedCourses.jsx
+++ src/Components/Body/MonetizedCourses/MonetizedCourses.jsx
@@ -5,7 +5,7 @@ function MonetizedCourses() {
   return (
     <section className="w-full px-4 bg-[#F5F7FD] py-24 flex justify-start items-center flex-col gap-8 overflow-hidden">
       <h3 className="max-md:text-3xl md:text-5xl font-medium">
-        سیستم <span className="line_after">مدیریت</span> دوره
+        <span className="line_after">درآمد</span> از دوره های آموزشی
       </h3>
       <p className="text-center text-[#7E7E7E]">اجومال با پلاگین تیوتر LMS یکپارچه شده است و دارای یک سیستم کامل برای مدیریت دوره های آموزشی است.</p>
       <MonetizedCoursesTabs />
