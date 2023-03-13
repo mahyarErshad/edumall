77cee90b8abf762925abebbd06cc76f6080a618f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 11:08:23 +0330

    Cube shape is added

--- src/Components/Body/ContentDrip/ContentDrip.jsx
+++ src/Components/Body/ContentDrip/ContentDrip.jsx
@@ -24,14 +24,14 @@ function ContentDrip() {
         <img src={frontImage} alt="Content drip" className="z-10 border absolute border-[#eee] rounded-[10px] shadow-md lg:h-[80%]" />
         <MouseParallaxContainer resetOnLeave={true} useWindowMouseEvents={true} globalFactorX={0.1} globalFactorY={0.1} containerStyle={{ overflow: "visible" }} className="absolute z-20 w-full h-full max-lg:hidden">
           <MouseParallaxChild className="absolute top-[20%] right-0 bg-gradient-to-b from-[#ABE54B] to-[#5EA82C] h-4 w-4 circle" inverted={true} />
-          <MouseParallaxChild className="absolute top-[10%] left-[10%]">
+          <MouseParallaxChild className="absolute top-[10%] left-[15%]">
             <img src={doubleCheck} alt="Double check" className="circle shadow-md" />
           </MouseParallaxChild>
           <MouseParallaxChild className="absolute top-[70%] right-[8%] p-6 bg-secondary rounded-[2rem] flex-center shadow-md">
             <FontAwesomeIcon color="#fff" className="w-[2.125rem] h-[2.125rem]" icon={faLock} />
           </MouseParallaxChild>
-          <MouseParallaxChild className="absolute top-[10%] left-[10%]">
-            <img src={doubleCheck} alt="Double check" className="circle shadow-md" />
+          <MouseParallaxChild className="absolute bottom-[10%] left-[10%]" inverted={true}>
+            <img src={shapeCube} alt="Double check" className="circle h-[2.1875rem] w-[2.1875rem]" />
           </MouseParallaxChild>
         </MouseParallaxContainer>
       </div>
