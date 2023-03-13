dbd857731e40b598859e85a29f4d391f568f3f60
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 08:58:03 +0330

    Repeat is responsive

--- src/Components/Body/ContentDrip/ContentDrip.jsx
+++ src/Components/Body/ContentDrip/ContentDrip.jsx
@@ -9,7 +9,7 @@ function ContentDrip() {
           <figure className="flex-center flex-col gap-4 max-w-[15.625rem]">
             <div className="flex-center w-full">
               <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">1</span>
-              <span className="flex-grow h-1 background-infinite-dots w-full"></span>
+              <span className="flex-grow h-1 background-infinite-dots max-md:bg-repeat-y md:bg-repeat-x w-full"></span>
             </div>
             <p className="font-medium">بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید.</p>
           </figure>
