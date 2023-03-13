8d9a4099525f8fd7556eaf57842661fb89ac1572
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 09:01:54 +0330

    Working on responsive

--- src/Components/Body/ContentDrip/ContentDrip.jsx
+++ src/Components/Body/ContentDrip/ContentDrip.jsx
@@ -6,7 +6,7 @@ function ContentDrip() {
       <div className="max-md:w-full md:w-[60%] flex-center flex-col">
         <h4 className="font-medium max-md:text-3xl md:text-5xl mb-16">جریان محتوا</h4>
         <div className="w-full flex-center">
-          <figure className="flex-center flex-col gap-4 max-w-[15.625rem]">
+          <figure className="flex-center flex-col gap-3 max-md:w-full md:max-w-[15.625rem]">
             <div className="flex-center w-full">
               <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">1</span>
               <span className="flex-grow h-1 background-infinite-dots max-md:bg-repeat-y md:bg-repeat-x w-full"></span>
