044f6906d220ec54c62497a165b0eee2cb8c4972
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 09:06:30 +0330

    Card is dynamic

--- src/Components/Body/ContentDrip/ContentDripCard.tsx
+++ src/Components/Body/ContentDrip/ContentDripCard.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-interface IProps{
+interface IProps {
   counter: string | number;
   text: string;
 }
 
-function ContentDripCard() {
+function ContentDripCard(props: IProps) {
   return (
     <figure className="flex-center flex-col gap-3 max-md:w-full md:max-w-[15.625rem]">
       <div className="flex-center w-full">
-        <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">1</span>
+        <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">{props.counter}</span>
         <span className="flex-grow h-1 background-infinite-dots max-md:bg-repeat-y md:bg-repeat-x w-full"></span>
       </div>
-      <p className="font-medium"></p>
+      <p className="font-medium">{props.text}</p>
     </figure>
   );
 }
