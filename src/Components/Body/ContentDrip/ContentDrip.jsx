02971d5d68ca95c0779bffaad0d0d882c5d69b6b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 09:09:20 +0330

    Created 3 cards

--- src/Components/Body/ContentDrip/ContentDrip.jsx
+++ src/Components/Body/ContentDrip/ContentDrip.jsx
@@ -7,7 +7,9 @@ function ContentDrip() {
       <div className="max-md:w-full md:w-[60%] flex-center flex-col">
         <h4 className="font-medium max-md:text-3xl md:text-5xl mb-16">جریان محتوا</h4>
         <div className="w-full flex-center">
-          <ContentDripCard counter="۱" text="بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید." hasDots />
+          <ContentDripCard counter="۰۱" text="بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید." hasDots />
+          <ContentDripCard counter="۰۲" text="بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید." hasDots />
+          <ContentDripCard counter="۰۳" text="بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید." />
         </div>
       </div>
       <div className="max-md:w-full md:w-[35%]"></div>
