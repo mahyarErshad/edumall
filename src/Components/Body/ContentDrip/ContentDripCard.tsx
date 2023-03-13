c664fbb8089c8344f4d8a354d07abd07d61024eb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 09:45:19 +0330

    Working for responsive

--- src/Components/Body/ContentDrip/ContentDripCard.tsx
+++ src/Components/Body/ContentDrip/ContentDripCard.tsx
@@ -10,7 +10,7 @@ interface IProps {
 function ContentDripCard(props: IProps) {
   return (
     <figure className="flex-center flex-col gap-3 max-md:w-full md:max-w-[15.625rem] max-md:mt-4">
-      <div className="flex-center w-full">
+      <div className="flex-center w-full max-md:flex-col max-md:items-start">
         <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">{props.counter}</span>
         <span className={`flex-grow h-1 ${props.hasDots && styles['background-infinite-dots']} max-md:bg-repeat-y md:bg-repeat-x w-full`}></span>
       </div>
