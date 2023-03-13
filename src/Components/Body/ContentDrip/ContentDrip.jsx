e320f736d8d498bbffaad8948a4994532bcd6576
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 08:41:57 +0330

    Wrappers are added

--- src/Components/Body/ContentDrip/ContentDrip.jsx
+++ src/Components/Body/ContentDrip/ContentDrip.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function ContentDrip() {
   return (
-    <div>ContentDrip</div>
-  )
+    <section className="w-full px-4 flex-center gap-8 max-md:flex-col pt-32 pb-20">
+      <div className="max-md:w-full md:w-[60%] flex-center flex-col gap-5">
+        <h4 className="font-medium max-md:text-3xl md:text-5xl">جریان محتوا</h4>
+        
+      </div>
+      <div className="max-md:w-full md:w-[35%]"></div>
+    </section>
+  );
 }
 
-export default ContentDrip
+export default ContentDrip;
