45caff6cf15ef4bb741fbb16926ecfe12d6cd487
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 09:21:42 +0330

    Using module css file

--- src/Components/Body/ContentDrip/ContentDripCard.tsx
+++ src/Components/Body/ContentDrip/ContentDripCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./styles.module.css"
+import styles from "./style.module.css"
 
 interface IProps {
   counter: string | number;
@@ -12,7 +12,7 @@ function ContentDripCard(props: IProps) {
     <figure className="flex-center flex-col gap-3 max-md:w-full max-w-[15.625rem]">
       <div className="flex-center w-full">
         <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">{props.counter}</span>
-        <span className={`flex-grow h-1 ${props.hasDots && "background-infinite-dots"} max-md:bg-repeat-y md:bg-repeat-x w-full`}></span>
+        <span className={`flex-grow h-1 ${props.hasDots && styles['background-infinite-dots']} max-md:bg-repeat-y md:bg-repeat-x w-full`}></span>
       </div>
       <p className="font-medium pl-7">{props.text}</p>
     </figure>
