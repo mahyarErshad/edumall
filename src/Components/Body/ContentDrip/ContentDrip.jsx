afb10fbfecaed600bb46d94c2ee11b5d1e45509a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 08:54:05 +0330

    P tag is styled

--- src/Components/Body/ContentDrip/ContentDrip.jsx
+++ src/Components/Body/ContentDrip/ContentDrip.jsx
@@ -6,12 +6,12 @@ function ContentDrip() {
       <div className="max-md:w-full md:w-[60%] flex-center flex-col gap-5">
         <h4 className="font-medium max-md:text-3xl md:text-5xl">جریان محتوا</h4>
         <div className="w-full flex-center">
-          <figure className="flex-center flex-col">
+          <figure className="flex-center flex-col max-w-[15.625rem]">
             <div className="flex-center w-full">
               <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">1</span>
               <span className="flex-grow h-1 background-infinite-dots w-full"></span>
             </div>
-            <p>بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید.</p>
+            <p className="font-medium">بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید.</p>
           </figure>
         </div>
       </div>
