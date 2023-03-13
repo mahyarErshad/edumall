b2ead3a023cda0ddcc5fe129b56a89c7ff5964fa
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 15:13:28 +0330

    Redesigned the component

--- src/Components/Body/MonetizedCourses/MonetizedCoursesTabs.jsx
+++ src/Components/Body/MonetizedCourses/MonetizedCoursesTabs.jsx
@@ -14,17 +14,17 @@ function MonetizedCoursesTabs() {
     <Tabs className="w-full flex-center max-md:flex-col gap-4 mt-10">
       <div className="max-lg:w-full lg:w-[49%]">
         <TabList className="flex flex-col justify-start h-full gap-3 flex-center lg:pr-[10%]">
-          <Tab className={`gap-4 w-full py-6 px-8 rounded-lg cursor-pointer ${styles.tab} tab`}>
+          <Tab className={`w-full py-6 px-8 rounded-lg cursor-pointer ${styles.tab} tab`}>
             <MoneyManagementIcon className=" transition-all duration-300" />
             <h3 className="text-black text-xl font-medium">مدیریت مالی</h3>
             <p className={`${styles.description} text-sm font-normal text-[#696969]`}>با ادغام WooCommerce و EDD، می توانید به راحتی پول خود را در داشبورد خود مدیریت کنید.</p>
           </Tab>
-          <Tab className={`gap-4 w-full py-6 px-8 rounded-lg cursor-pointer ${styles.tab} tab`}>
+          <Tab className={`w-full py-6 px-8 rounded-lg cursor-pointer ${styles.tab} tab`}>
             <MultiplePayment className="transition-all duration-300" />
             <h3 className="text-black text-xl font-medium">روش های پرداخت چندگانه</h3>
             <p className={`${styles.description} text-sm font-normal text-[#696969]`}>اجومال محبوب ترین روش های پرداخت را در اختیار کاربران قرار می دهد تا به همه کاربران کمک کند تا تراکنش های یکپارچه داشته باشند.</p>
           </Tab>
-          <Tab className={`gap-4 w-full py-6 px-8 rounded-lg cursor-pointer ${styles.tab} tab`}>
+          <Tab className={`w-full py-6 px-8 rounded-lg cursor-pointer ${styles.tab} tab`}>
             <LifetimeAccess className="transition-all duration-300" />
             <h3 className="text-black text-xl font-medium">دسترسی مادام العمر</h3>
             <p className={`${styles.description} text-sm font-normal text-[#696969]`}>به لطف دسترسی مادام العمر ارائه شده از تیوتر LMS، دانشجویان همیشه می توانند به دوره قبل از ثبت نام، دسترسی داشته باشند.</p>
