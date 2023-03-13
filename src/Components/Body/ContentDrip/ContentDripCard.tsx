0f4e39b18d7e048170b12684f252ace8a74868de
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 09:43:50 +0330

    Cards are responsive on small screens

--- src/Components/Body/ContentDrip/ContentDripCard.tsx
+++ src/Components/Body/ContentDrip/ContentDripCard.tsx
@@ -9,7 +9,7 @@ interface IProps {
 
 function ContentDripCard(props: IProps) {
   return (
-    <figure className="flex-center flex-col gap-3 max-md:w-full max-w-[15.625rem]">
+    <figure className="flex-center flex-col gap-3 max-md:w-full md:max-w-[15.625rem] max-md:mt-4">
       <div className="flex-center w-full">
         <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">{props.counter}</span>
         <span className={`flex-grow h-1 ${props.hasDots && styles['background-infinite-dots']} max-md:bg-repeat-y md:bg-repeat-x w-full`}></span>
