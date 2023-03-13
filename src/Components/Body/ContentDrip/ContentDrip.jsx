8f9f564d72c4cf63d38eae4c1ef11b6b32c4b546
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 08:48:11 +0330

    Styling the card

--- src/Components/Body/ContentDrip/ContentDrip.jsx
+++ src/Components/Body/ContentDrip/ContentDrip.jsx
@@ -5,7 +5,15 @@ function ContentDrip() {
     <section className="w-full px-4 flex-center gap-8 max-md:flex-col pt-32 pb-20">
       <div className="max-md:w-full md:w-[60%] flex-center flex-col gap-5">
         <h4 className="font-medium max-md:text-3xl md:text-5xl">جریان محتوا</h4>
-        
+        <div className="w-full flex-center">
+          <figure className="flex-center flex-col">
+            <div className="flex-center">
+              <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">1</span>
+              <span></span>
+            </div>
+            <p>بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید.</p>
+          </figure>
+        </div>
       </div>
       <div className="max-md:w-full md:w-[35%]"></div>
     </section>
