6730baf15599d5265b5d635e8d8607da3c64c36f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 09:05:02 +0330

    Using Card as a component

--- src/Components/Body/ContentDrip/ContentDripCard.tsx
+++ src/Components/Body/ContentDrip/ContentDripCard.tsx
@@ -7,7 +7,7 @@ function ContentDripCard() {
         <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">1</span>
         <span className="flex-grow h-1 background-infinite-dots max-md:bg-repeat-y md:bg-repeat-x w-full"></span>
       </div>
-      <p className="font-medium">بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید.</p>
+      <p className="font-medium"></p>
     </figure>
   );
 }
