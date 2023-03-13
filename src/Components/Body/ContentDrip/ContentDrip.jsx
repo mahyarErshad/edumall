75a0e2bbb4360c9c729d3ed1b63e88ed4b1b9aac
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 11:17:17 +0330

    Changed the layout

--- src/Components/Body/ContentDrip/ContentDrip.jsx
+++ src/Components/Body/ContentDrip/ContentDrip.jsx
@@ -10,31 +10,36 @@ import { faLock } from "@fortawesome/free-solid-svg-icons";
 
 function ContentDrip() {
   return (
-    <section className="w-full px-4 flex-center md:gap-8 max-md:flex-col py-8">
-      <div className="max-md:w-full md:w-[60%] flex-center flex-col">
-        <h4 className="font-medium max-md:text-3xl md:text-5xl mb-16">جریان محتوا</h4>
-        <div className="w-full flex-center max-md:flex-col">
-          <ContentDripCard counter="۰۱" text="بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید." hasDots />
-          <ContentDripCard counter="۰۲" text="دانش آموزان تا پایان درس قبلی نمی توانند به درس بعدی دسترسی داشته باشند." hasDots />
-          <ContentDripCard counter="۰۳" text="مدیر، مربیان می توانند درس را تا زمان شروع قفل کنند." />
+    <section className="w-full py-8 px-4 flex-center flex-col gap-10">
+      <section className="w-full flex-center md:gap-8 max-md:flex-col">
+        <div className="max-md:w-full md:w-[60%] flex-center flex-col">
+          <h4 className="font-medium max-md:text-3xl md:text-5xl mb-16">جریان محتوا</h4>
+          <div className="w-full flex-center max-md:flex-col">
+            <ContentDripCard counter="۰۱" text="بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید." hasDots />
+            <ContentDripCard counter="۰۲" text="دانش آموزان تا پایان درس قبلی نمی توانند به درس بعدی دسترسی داشته باشند." hasDots />
+            <ContentDripCard counter="۰۳" text="مدیر، مربیان می توانند درس را تا زمان شروع قفل کنند." />
+          </div>
         </div>
-      </div>
-      <div className="max-md:w-full md:w-[35%] relative h-[31.25rem] flex-center max-md:mt-6">
-        <img src={borderImage} alt="a circle made by dots for border" className="absolute z-0 lg:h-full" />
-        <img src={frontImage} alt="Content drip" className="z-10 border absolute border-[#eee] rounded-[10px] shadow-md lg:h-[80%]" />
-        <MouseParallaxContainer resetOnLeave={true} useWindowMouseEvents={true} globalFactorX={0.1} globalFactorY={0.1} containerStyle={{ overflow: "visible" }} className="absolute z-20 w-full h-full max-lg:hidden">
-          <MouseParallaxChild className="absolute top-[20%] right-[10%] bg-gradient-to-b from-[#ABE54B] to-[#5EA82C] h-4 w-4 circle" inverted={true} />
-          <MouseParallaxChild className="absolute top-[10%] left-[15%]">
-            <img src={doubleCheck} alt="Double check" className="circle shadow-md" />
-          </MouseParallaxChild>
-          <MouseParallaxChild className="absolute top-[70%] right-[8%] p-6 bg-secondary rounded-[2rem] flex-center shadow-md">
-            <FontAwesomeIcon color="#fff" className="w-[2.125rem] h-[2.125rem]" icon={faLock} />
-          </MouseParallaxChild>
-          <MouseParallaxChild className="absolute bottom-[10%] left-[13%]" inverted={true}>
-            <img src={shapeCube} alt="Double check" className="circle h-[2.1875rem] w-[2.1875rem]" />
-          </MouseParallaxChild>
-        </MouseParallaxContainer>
-      </div>
+        <div className="max-md:w-full md:w-[35%] relative h-[31.25rem] flex-center max-md:mt-6">
+          <img src={borderImage} alt="a circle made by dots for border" className="absolute z-0 lg:h-full" />
+          <img src={frontImage} alt="Content drip" className="z-10 border absolute border-[#eee] rounded-[10px] shadow-md lg:h-[80%]" />
+          <MouseParallaxContainer resetOnLeave={true} useWindowMouseEvents={true} globalFactorX={0.1} globalFactorY={0.1} containerStyle={{ overflow: "visible" }} className="absolute z-20 w-full h-full max-lg:hidden">
+            <MouseParallaxChild className="absolute top-[20%] right-[10%] bg-gradient-to-b from-[#ABE54B] to-[#5EA82C] h-4 w-4 circle" inverted={true} />
+            <MouseParallaxChild className="absolute top-[10%] left-[15%]">
+              <img src={doubleCheck} alt="Double check" className="circle shadow-md" />
+            </MouseParallaxChild>
+            <MouseParallaxChild className="absolute top-[70%] right-[8%] p-6 bg-secondary rounded-[2rem] flex-center shadow-md">
+              <FontAwesomeIcon color="#fff" className="w-[2.125rem] h-[2.125rem]" icon={faLock} />
+            </MouseParallaxChild>
+            <MouseParallaxChild className="absolute bottom-[10%] left-[13%]" inverted={true}>
+              <img src={shapeCube} alt="Double check" className="circle h-[2.1875rem] w-[2.1875rem]" />
+            </MouseParallaxChild>
+          </MouseParallaxContainer>
+        </div>
+      </section>
+      <section className="bg-red-500 h-20 w-full">
+
+      </section>
     </section>
   );
 }
