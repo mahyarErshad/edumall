de64cf2bc43c1c9b1faa98dae0dc505003a9e1f5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-13 08:25:41 +0330

    Imported ContentDrip

--- src/Components/Body/MainBody/MainBody.jsx
+++ src/Components/Body/MainBody/MainBody.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import HeroSection from "../../Header/HeroSection/HeroSection";
 import DemoPages from "../DemoPages/DemoPages";
 import WhyEdumall from "../WhyEdumall/WhyEdumall";
-import ToturSection from "../TutorSection/TutorSection"
+import ToturSection from "../TutorSection/TutorSection";
 import ZoomVideo from "../ZoomVideo/ZoomVideo";
 import CourseManagementSystem from "../CourseManagementSystem/CourseManagementSystem";
 import AdministrationsOption from "../AdministrationsOption/AdministrationsOption";
 import ELearningManagement from "../ELearningManagement/ELearningManagement";
+import ContentDrip from "../ContentDrip/ContentDrip";
 
 function MainBody() {
   return (
@@ -19,6 +20,7 @@ function MainBody() {
       <CourseManagementSystem />
       <AdministrationsOption />
       <ELearningManagement />
+      <ContentDrip />
     </>
   );
 }
