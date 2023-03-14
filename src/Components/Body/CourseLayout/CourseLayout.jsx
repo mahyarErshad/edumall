53597a7b66c2bccdc78cac030b641466094ef2a8
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-14 11:33:35 +0330

    Containers are added

--- src/Components/Body/CourseLayout/CourseLayout.jsx
+++ src/Components/Body/CourseLayout/CourseLayout.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 function CourseLayout() {
-  return <section className="px-4 py-10 w-full flex flex-col ">
-    <div className="flex ">
-
-    </div>
-  </section>;
+  return (
+    <section className="px-4 py-10 w-full flex flex-col bg-navy">
+      <div className="flex-center max-md:flex-col-reverse">
+        <div className="flex justify-start max-md:w-full md:w-2/4"></div>
+        <div className="flex justify-start max-md:w-full md:w-2/4"></div>
+      </div>
+    </section>
+  );
 }
 
 export default CourseLayout;
