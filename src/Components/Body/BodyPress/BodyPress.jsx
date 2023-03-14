3649d7c209d24e9994e6eb30ea7d48b1f66fad4b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-14 09:20:54 +0330

    Margin bottom is 28px

--- src/Components/Body/BodyPress/BodyPress.jsx
+++ src/Components/Body/BodyPress/BodyPress.jsx
@@ -5,7 +5,7 @@ function BodyPress() {
     <section className="w-full px-4 bg-gradient-to-b from-[#1600C3] to-[#AE6DD7] py-10 flex-center max-md:flex-col">
       <div className="max-md:w-full md:w-[40%] flex-center">
         <div className="flex flex-col justify-start">
-          <span className="py-1 px-4 bg-gradient-to-bl from-[#F646A9] to-[#5C51FF] text-white font-medium text-lg w-fit rounded-md">جامعه</span>
+          <span className="py-1 px-4 bg-gradient-to-bl from-[#F646A9] to-[#5C51FF] text-white font-medium text-lg w-fit rounded-md mb-7">جامعه</span>
         </div>
       </div>
       <div className="max-md:w-full md:w-[60%]"></div>
