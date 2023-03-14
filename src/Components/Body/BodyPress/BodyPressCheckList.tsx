101265d5bcf162edec752cc4effd1295476f4bca
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-14 10:37:53 +0330

    Logo has flex-shrink-0

--- src/Components/Body/BodyPress/BodyPressCheckList.tsx
+++ src/Components/Body/BodyPress/BodyPressCheckList.tsx
@@ -8,7 +8,7 @@ interface IProps {
 function BodyPressCheckList(props: IProps) {
   return (
     <div className="flex items-center gap-2 w-full">
-      <span className="circle w-[1.875rem] h-[1.875rem] bg-[#6CBD7E] flex-center">
+      <span className="circle w-[1.875rem] h-[1.875rem] bg-[#6CBD7E] flex-center flex-shrink-0">
         <FontAwesomeIcon size="lg" color="#fff" icon={faCheck} />
       </span>
       <p className="text-white text-right text-base font-medium">{props.text}</p>
