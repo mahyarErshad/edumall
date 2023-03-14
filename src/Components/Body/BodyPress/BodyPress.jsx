93a38346124765b723796b19b17d257220228dea
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-14 09:37:02 +0330

    Created module css file

--- src/Components/Body/BodyPress/BodyPress.jsx
+++ src/Components/Body/BodyPress/BodyPress.jsx
@@ -4,10 +4,10 @@ function BodyPress() {
   return (
     <section className="w-full px-4 bg-gradient-to-b from-[#1600C3] to-[#AE6DD7] py-10 flex-center max-md:flex-col">
       <div className="max-md:w-full md:w-[40%] flex-center">
-        <div className="flex flex-col justify-start">
+        <div className="flex flex-col justify-start max-w-[27.1875rem]">
           <span className="py-1 px-4 bg-gradient-to-bl from-[#F646A9] to-[#5C51FF] text-white font-medium text-lg w-fit rounded-md mb-7">جامعه</span>
-          <h3 className="font-medium max-md:text-xl md:text-5xl text-white leading-5">
-            بهترین جامعه آموزشی را با <span>بادی پرس</span> بسازید
+          <h3 className="font-medium max-md:text-xl md:text-5xl text-white">
+            بهترین جامعه آموزشی را با <mark className="">بادی پرس</mark> بسازید
           </h3>
         </div>
       </div>
