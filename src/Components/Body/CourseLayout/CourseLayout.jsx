6a4758058678379b8c2c0c6df3bbc641102e4db4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-14 12:34:29 +0330

    Container resized

--- src/Components/Body/CourseLayout/CourseLayout.jsx
+++ src/Components/Body/CourseLayout/CourseLayout.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import styles from "./style.module.css"
+import styles from "./style.module.css";
 
 function CourseLayout() {
   return (
     <section className="px-4 py-10 w-full flex flex-col bg-navy">
-      <div className="flex-center max-md:flex-col-reverse">
-        <div className="flex justify-start max-md:w-full md:w-2/4">
-            <span className={`${styles.counter} line-after`}>+۰۷</span>
+      <div className="max-lg:w-full lg:w-[40%] flex-center max-md:flex-col-reverse">
+        <div className="flex justify-start max-w-[34.125rem]">
+          <span className={`${styles.counter} line-after`}>+۰۷</span>
         </div>
         <div className="flex justify-start max-md:w-full md:w-2/4"></div>
       </div>
