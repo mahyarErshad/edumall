cbcf60596934d14d43ed98a7db887dd9ea034294
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-14 09:57:22 +0330

    Added check icon

--- src/Components/Body/BodyPress/BodyPressCheckList.tsx
+++ src/Components/Body/BodyPress/BodyPressCheckList.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-interface IProps{
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+interface IProps {
   text: string;
 }
 
-function BodyPressCheckList(props:IProps) {
+function BodyPressCheckList(props: IProps) {
   return (
-    <div>
-        
+    <div className="flex-center gap-4 w-full">
+      <span className="circle w-[1.875rem] h-[1.875rem] bg-[#6CBD7E] flex-center">
+        <FontAwesomeIcon icon={faCheck} />
+      </span>
     </div>
-  )
+  );
 }
 
-export default BodyPressCheckList
+export default BodyPressCheckList;
