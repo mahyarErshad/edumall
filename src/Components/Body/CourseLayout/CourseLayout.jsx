ba279f5361dfa60f09609466cc68d624b2347d4d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-14 11:30:41 +0330

    Defined navy color

--- src/Components/Body/CourseLayout/CourseLayout.jsx
+++ src/Components/Body/CourseLayout/CourseLayout.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function CourseLayout() {
-  return <section className="px-4 py-10 w-full flex flex-col bg-[#031F42]">CourseLayout</section>;
+  return <section className="px-4 py-10 w-full flex flex-col ">
+    <div className="flex ">
+
+    </div>
+  </section>;
 }
 
 export default CourseLayout;
