0587371e827915f6a06ce11401c12e18650b9bad
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-14 08:58:40 +0330

    Wrapper of the text section is done

--- src/Components/Body/BodyPress/BodyPress.jsx
+++ src/Components/Body/BodyPress/BodyPress.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function BodyPress() {
   return (
-    <section className='w-full px-4 bg-gradient-to-b from-[#1600C3] to-[#AE6DD7] pt-10 flex-center max-md:flex-col'>
+    <section className="w-full px-4 bg-gradient-to-b from-[#1600C3] to-[#AE6DD7] pt-10 flex-center max-md:flex-col">
+      <div className="max-md:w-full md:w-[30%] flex flex-col justify-start md:pl-4">
         
+      </div>
+      <div className="max-md:w-full md:w-[70%]"></div>
     </section>
-  )
+  );
 }
 
-export default BodyPress
+export default BodyPress;
