9f751e3fbf8279dcf24097ea6f981adaf86b074c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-14 11:27:24 +0330

    Initialized the section

--- src/Components/Body/CourseLayout/CourseLayout.jsx
+++ src/Components/Body/CourseLayout/CourseLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function CourseLayout() {
-  return <div>CourseLayout</div>;
+  return <section className="px-4 py-10 w-full flex flex-col bg-[#031F42]">CourseLayout</section>;
 }
 
 export default CourseLayout;
