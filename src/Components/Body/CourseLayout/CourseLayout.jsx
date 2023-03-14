efb5b31428b96f628b20f40db514fdc0a213c6af
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-14 11:36:54 +0330

    Added the title

--- src/Components/Body/CourseLayout/CourseLayout.jsx
+++ src/Components/Body/CourseLayout/CourseLayout.jsx
@@ -4,7 +4,9 @@ function CourseLayout() {
   return (
     <section className="px-4 py-10 w-full flex flex-col bg-navy">
       <div className="flex-center max-md:flex-col-reverse">
-        <div className="flex justify-start max-md:w-full md:w-2/4"></div>
+        <div className="flex justify-start max-md:w-full md:w-2/4">
+            <span className={` line-after`}>+۰۷</span>
+        </div>
         <div className="flex justify-start max-md:w-full md:w-2/4"></div>
       </div>
     </section>
