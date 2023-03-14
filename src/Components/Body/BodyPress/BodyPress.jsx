e77d361cef2341ce5a3cde65944f2397e08d187d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-14 09:19:28 +0330

    Moved texts to center

--- src/Components/Body/BodyPress/BodyPress.jsx
+++ src/Components/Body/BodyPress/BodyPress.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 function BodyPress() {
   return (
-    <section className="w-full px-4 bg-gradient-to-b from-[#1600C3] to-[#AE6DD7] pt-10 flex-center max-md:flex-col">
-      <div className="max-md:w-full md:w-[30%] flex flex-col justify-start md:pl-4">
-        
+    <section className="w-full px-4 bg-gradient-to-b from-[#1600C3] to-[#AE6DD7] py-10 flex-center max-md:flex-col">
+      <div className="max-md:w-full md:w-[40%] flex-center">
+        <div className="flex flex-col justify-start">
+          <span className="py-1 px-4 bg-gradient-to-bl from-[#F646A9] to-[#5C51FF] text-white font-medium text-lg w-fit rounded-md">جامعه</span>
+        </div>
       </div>
-      <div className="max-md:w-full md:w-[70%]"></div>
+      <div className="max-md:w-full md:w-[60%]"></div>
     </section>
   );
 }
