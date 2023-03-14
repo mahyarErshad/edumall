44ecbca12a873769e18561fc9109fbe25c350194
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-14 09:44:32 +0330

    Max width increased to 500px

--- src/Components/Body/BodyPress/BodyPress.jsx
+++ src/Components/Body/BodyPress/BodyPress.jsx
@@ -5,7 +5,7 @@ function BodyPress() {
   return (
     <section className="w-full px-4 bg-gradient-to-b from-[#1600C3] to-[#AE6DD7] py-10 flex-center max-md:flex-col">
       <div className="max-md:w-full md:w-[40%] flex-center">
-        <div className="flex flex-col justify-start max-w-[27.1875rem]">
+        <div className="flex flex-col justify-start max-w-[31.25rem]">
           <span className="py-1 px-4 bg-gradient-to-bl from-[#F646A9] to-[#5C51FF] text-white font-medium text-lg w-fit rounded-md mb-7">جامعه</span>
           <h3 className={`${styles["line-height"]} font-medium max-md:text-xl md:text-5xl text-white`}>
             بهترین جامعه آموزشی را با <mark className={styles["text-shadow"]}>بادی پرس</mark> بسازید
