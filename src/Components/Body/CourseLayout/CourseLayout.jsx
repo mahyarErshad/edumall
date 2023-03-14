074d4a309310a5c8ff65350a89458ff716d9542c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-14 12:34:43 +0330

    Added line_after class

--- src/Components/Body/CourseLayout/CourseLayout.jsx
+++ src/Components/Body/CourseLayout/CourseLayout.jsx
@@ -6,7 +6,7 @@ function CourseLayout() {
     <section className="px-4 py-10 w-full flex flex-col bg-navy">
       <div className="max-lg:w-full lg:w-[40%] flex-center max-md:flex-col-reverse">
         <div className="flex justify-start max-w-[34.125rem]">
-          <span className={`${styles.counter} line-after`}>+۰۷</span>
+          <span className={`${styles.counter} line_after`}>+۰۷</span>
         </div>
         <div className="flex justify-start max-md:w-full md:w-2/4"></div>
       </div>
