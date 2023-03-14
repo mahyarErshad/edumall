2e841d279f4eb8ecce58ac7ceb9d2b45a65d4d8c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-03-14 08:50:52 +0330

    pt = 40px

--- src/Components/Body/BodyPress/BodyPress.jsx
+++ src/Components/Body/BodyPress/BodyPress.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function BodyPress() {
   return (
-    <section className='w-full px-4 bg-gradient-to-b from-[#1600C3] to-[#AE6DD7] py-32'>BodyPress</section>
+    <section className='w-full px-4 bg-gradient-to-b from-[#1600C3] to-[#AE6DD7] pt-10'>BodyPress</section>
   )
 }
 
