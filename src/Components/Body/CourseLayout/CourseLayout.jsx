8bf636c179603b1df3e371ca3a1f0c21688436dd
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-03 10:21:24 +0430

    Added another shape

--- src/Components/Body/CourseLayout/CourseLayout.jsx
+++ src/Components/Body/CourseLayout/CourseLayout.jsx
@@ -18,15 +18,12 @@ function CourseLayout() {
           <img src={laptopImage} alt="An open laptop showing courses" />
         </div>
       </div>
-      <MouseParallaxContainer containerStyle={{position:"absolute", overflow:"visible"}} resetOnLeave={true} useWindowMouseEvents={true} globalFactorX={0.1} globalFactorY={0.1} className="max-lg:hidden h-full w-full">
+      <MouseParallaxContainer containerStyle={{ position: "absolute", overflow: "visible" }} resetOnLeave={true} useWindowMouseEvents={true} globalFactorX={0.1} globalFactorY={0.1} className="max-lg:hidden h-full w-full">
         <MouseParallaxChild className="absolute top-[-8.125rem] left-[1.4375rem] bg-transparent border border-white circle w-[28.625rem] h-[28.625rem] opacity-40" />
         <MouseParallaxChild className="absolute top-0 left-[10rem] bg-transparent border border-white circle w-[12rem] h-[12rem] opacity-40" inverted={true} />
         <MouseParallaxChild className="absolute top-[-8.125rem] right-[1.4375rem] bg-transparent border border-white circle w-[28.625rem] h-[28.625rem] opacity-40" />
         <MouseParallaxChild className="absolute top-0 right-[10rem] bg-transparent border border-white circle w-[12rem] h-[12rem] opacity-40" inverted={true} />
-        <MouseParallaxChild className="absolute top-[28%] right-[44%] bg-gradient-to-b from-[#FDE190] to-[#FEC93D] h-[0.9375rem] w-[0.9375rem] circle" />
-        <MouseParallaxChild className="absolute top-[33%] right-[38%] bg-gradient-to-b from-[#ABE54B] to-[#5EA82C] h-[2.5rem] w-[2.5rem] circle" inverted={true} />
-        <MouseParallaxChild className="bg-gradient-to-b from-[#FDE190] to-[#FEC93D] h-[0.9375rem] w-[0.9375rem] absolute bottom-[12.5rem] left-[7.75rem] circle" inverted={true} />
-        <MouseParallaxChild className="bg-gradient-to-b from-[#F9CC57] to-[#F87924] h-[2.5rem] w-[2.5rem] absolute bottom-[17rem] left-[5rem] circle" />
+        <MouseParallaxChild className="absolute left-[9.8125rem] top-[18.0625rem] bg-gradient-to-b from-[#529CFD] to-[#75C9FD] h-[3.375rem] w-[3.375rem] circle" />
       </MouseParallaxContainer>
     </section>
   );
