e4d4347c8981bf62dea65c50f64010253cc72406
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-10 13:46:43 +0430

    Changed the text color

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="w-full bg-[#F8F8F8] px-4 flex justify-start items-center flex-col py-24">
-      <h3 className="text-3xl font-medium text-secondary">راه حلی هوشمند برای پلتفرم آموزش الکترونیکی</h3>
+      <h3 className="text-3xl font-medium text-navy">راه حلی هوشمند برای پلتفرم آموزش الکترونیکی</h3>
     </footer>
   );
 }
