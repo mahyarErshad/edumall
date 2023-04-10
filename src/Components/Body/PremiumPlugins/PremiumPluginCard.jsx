0d3f435acf8aefa22fa17fa60feda79e7ff5a996
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-10 12:38:32 +0430

    PremiumPluginCard is created

--- src/Components/Body/PremiumPlugins/PremiumPluginCard.jsx
+++ src/Components/Body/PremiumPlugins/PremiumPluginCard.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
-function PremiumPluginCard() {
+function PremiumPluginCard(props) {
   return (
-    <div>PremiumPluginCard</div>
-  )
+    <div className="flex-center gap-5 flex-col">
+      <img className="rounded-[3.125rem]" src={props.image} alt={props.text} />
+      <h4 className="text-base text-white font-medium">{props.text}</h4>
+    </div>
+  );
 }
 
-export default PremiumPluginCard
+export default PremiumPluginCard;
