a5aca49233dd500ba8e8a496ac9be60cabb82476
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-10 13:54:20 +0430

    P tag is responsive

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Footer() {
   return (
     <footer className="w-full bg-[#F8F8F8] px-4 flex justify-start items-center flex-col py-24 gap-8">
-      <h3 className="text-3xl font-medium text-primary">راه حلی هوشمند برای پلتفرم آموزش الکترونیکی</h3>
-      <p className="max-md:text-[1.75rem] md:text-[3rem] font-medium text-navy max-w-3xl text-center leading-[3.5rem]">امروز وب سایت آموزش خود را با اجومال راه اندازی کنید</p>
+      <h3 className="text-3xl text-center font-medium text-primary">راه حلی هوشمند برای پلتفرم آموزش الکترونیکی</h3>
+      <p className="max-md:text-[1.75rem] md:text-[3rem] font-medium text-navy max-w-3xl text-center max-md:leading-[2.5rem] md:leading-[3.5rem]">امروز وب سایت آموزش خود را با اجومال راه اندازی کنید</p>
     </footer>
   );
 }
