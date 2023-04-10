e77e984b74dd9740e4e3e5749e095418195cfc20
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-10 12:08:41 +0430

    Created component for WooCommerceIntegrationCard

--- src/Components/Body/WooCommerceIntegration/WooCommerceIntegration.jsx
+++ src/Components/Body/WooCommerceIntegration/WooCommerceIntegration.jsx
@@ -7,9 +7,13 @@ function WooCommerceIntegration() {
       <div className="max-md:w-full md:w-2/4 flex justify-end items-start">
         <img src={image} alt="WooCommerce Integration" className="shadow-md border-[0.625rem] rounded-[1.25rem] border-black self-start" />
       </div>
-      <div className="max-md:w-full md:w-2/4 flex justify-start items-start">
+      <div className="max-md:w-full md:w-2/4 flex flex-col justify-start items-start">
         <h3 className="font-medium max-md:text-[1.75rem] md:text-5xl line_after mb-[4.6875rem]">ادغام ووکامرس</h3>
-        
+        <div className="w-full flex flex-col gap-2">
+          <ContentDripCard counter="۰۱" text="بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید." />
+          <ContentDripCard counter="۰۲" text="دانش آموزان تا پایان درس قبلی نمی توانند به درس بعدی دسترسی داشته باشند." />
+          <ContentDripCard counter="۰۳" text="مدیر، مربیان می توانند درس را تا زمان شروع قفل کنند." />
+        </div>
       </div>
     </section>
   );
---
+++ src/Components/Body/WooCommerceIntegration/WooCommerceIntegrationCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "../ContentDrip/style.module.css"
+
+
+function WooCommerceIntegrationCard(props) {
+  return (
+    <figure className="md:flex-center max-md:flex md:flex-col gap-3 max-md:w-full md:max-w-[15.625rem] max-md:mt-4">
+      <div className="flex-center md:w-full max-md:flex-col">
+        <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">{props.counter}</span>
+        <span className={`flex-grow h-1 ${props.hasDots && styles['background-infinite-dots']} max-md:mt-4 max-md:bg-repeat-y md:bg-repeat-x max-md:h-8 max-md:w-2 md:w-full`}></span>
+      </div>
+      <p className="font-medium pl-7 max-md:self-start max-md:translate-y-2 min-h-[4.5rem] mt-2">{props.text}</p>
+    </figure>
+  );
+}
+
+export default WooCommerceIntegrationCard;
