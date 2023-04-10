5aeda20ca51e4c024cf7bbb32d5ab3a8ee1e3f10
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-10 16:07:51 +0430

    y axis dots are done in WooCommerceIntegrationCard

--- src/Components/Body/WooCommerceIntegration/WooCommerceIntegration.jsx
+++ src/Components/Body/WooCommerceIntegration/WooCommerceIntegration.jsx
@@ -9,7 +9,7 @@ import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 function WooCommerceIntegration() {
   return (
     <section className="py-[7.5rem] flex max-md:flex-col max-md:gap-8 md:gap-24">
-      <div className="max-md:w-full md:w-2/4 flex justify-end items-start relative">
+      <div className="max-md:w-full md:w-2/4 flex justify-center md:justify-end items-start relative">
         <MouseParallaxContainer containerStyle={{ position: "absolute", overflow: "visible" }} resetOnLeave={true} useWindowMouseEvents={true} globalFactorX={0.1} globalFactorY={0.1} className="max-lg:hidden h-full w-full">
           <MouseParallaxChild className="bg-gradient-to-b from-[#AFE84F] to-[#55A229] h-5 w-5 absolute right-[20%] circle" inverted={true} />
           <MouseParallaxChild className="absolute left-0 top-0" inverted={true}>
@@ -23,12 +23,12 @@ function WooCommerceIntegration() {
         <img src={image} alt="WooCommerce Integration" className="shadow-md border-[0.625rem] rounded-[1.25rem] border-black self-start" />
       </div>
       <div className="max-md:w-full md:w-2/4 flex flex-col justify-start items-start">
-        <h3 className="font-medium max-md:text-[1.75rem] md:text-5xl mb-[4.6875rem]">
+        <h3 className="font-medium max-md:self-center max-md:text-[1.75rem] md:text-5xl mb-[4.6875rem]">
           ادغام <strong className="line_after">ووکامرس</strong>
         </h3>
         <div className="w-full flex flex-col">
-          <WooCommerceIntegrationCard counter="۰۱" text="فروش هر چیزی از محصولات دیجیتال گرفته تا فیزیکی با افزونه ووکامرس." />
-          <WooCommerceIntegrationCard counter="۰۲" text="راه اندازی سیستم پرداخت با محبوب ترین روش های پرداخت: پی پال، استریپ، حواله بانکی." />
+          <WooCommerceIntegrationCard counter="۰۱" text="فروش هر چیزی از محصولات دیجیتال گرفته تا فیزیکی با افزونه ووکامرس." hasDots />
+          <WooCommerceIntegrationCard counter="۰۲" text="راه اندازی سیستم پرداخت با محبوب ترین روش های پرداخت: پی پال، استریپ، حواله بانکی." hasDots />
           <WooCommerceIntegrationCard counter="۰۳" text="داشتن گزارش کامل از فروش خود در هر زمان." />
         </div>
       </div>
--- src/Components/Body/WooCommerceIntegration/WooCommerceIntegrationCard.jsx
+++ src/Components/Body/WooCommerceIntegration/WooCommerceIntegrationCard.jsx
@@ -4,10 +4,10 @@ import styles from "../ContentDrip/style.module.css"
 
 function WooCommerceIntegrationCard(props) {
   return (
-    <figure className="md:flex-center max-md:flex md:flex-col gap-3 w-full max-md:mt-4">
-      <div className="flex-center md:w-full max-md:flex-col">
+    <figure className="flex gap-3 w-full max-md:mt-4">
+      <div className="flex-center flex-col">
         <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">{props.counter}</span>
-        <span className={`flex-grow h-1 ${props.hasDots && styles['background-infinite-dots']} max-md:mt-4 max-md:bg-repeat-y md:bg-repeat-x max-md:h-8 max-md:w-2 md:w-full`}></span>
+        <span className={`flex-grow h-1 ${props.hasDots && styles['background-infinite-dots-y']} md:py-6 max-md:mt-4 bg-repeat-y max-md:h-8 max-md:w-2 md:w-full`}></span>
       </div>
       <p className="font-medium pl-7 max-md:self-start max-md:translate-y-2 min-h-[4.5rem] mt-2">{props.text}</p>
     </figure>
