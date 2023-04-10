a29d1ff93026d6166464bee4d45ee0b224e39821
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-10 12:00:46 +0430

    h3 is added and styled

--- src/Components/Body/WooCommerceIntegration/WooCommerceIntegration.jsx
+++ src/Components/Body/WooCommerceIntegration/WooCommerceIntegration.jsx
@@ -4,7 +4,13 @@ import image from "../../../assets/images/WooCommerceIntegration.jpg";
 function WooCommerceIntegration() {
   return (
     <section className="py-[7.5rem] flex max-md:flex-col md:gap-24">
-      <img src={image} alt="WooCommerce Integration" className="shadow-md border-[0.625rem] rounded-[1.25rem] border-black" />
+      <div className="max-md:w-full md:w-2/4 flex justify-end items-start">
+        <img src={image} alt="WooCommerce Integration" className="shadow-md border-[0.625rem] rounded-[1.25rem] border-black self-start" />
+      </div>
+      <div className="max-md:w-full md:w-2/4 flex justify-start items-start">
+        <h3 className="font-medium max-md:text-[1.75rem] md:text-5xl line_after mb-[4.6875rem]">ادغام ووکامرس</h3>
+        
+      </div>
     </section>
   );
 }
