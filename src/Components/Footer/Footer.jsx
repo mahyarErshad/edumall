8cd6db2faf9e731c3ab9c34839de2c9fccf79ef6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-10 13:45:17 +0430

    H3 added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <footer className='w-full bg-[#F8F8F8] px-4 flex-center flex-col py-24'>
-    
+    <footer className="w-full bg-[#F8F8F8] px-4 flex justify-start items-center flex-col py-24">
+      <h3 className="text-3xl font-medium text-secondary">راه حلی هوشمند برای پلتفرم آموزش الکترونیکی</h3>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
