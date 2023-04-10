1f1f1398fc7bb8c59044f9c152497aebb328ed80
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-10 11:54:26 +0430

    Image added

--- src/Components/Body/WooCommerceIntegration/WooCommerceIntegration.jsx
+++ src/Components/Body/WooCommerceIntegration/WooCommerceIntegration.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import image from "../../../assets/images/WooCommerceIntegration.jpg";
 
 function WooCommerceIntegration() {
-  return <section className="py-[7.5rem] flex max-md:flex-col md:gap-24">
-    
-  </section>;
+  return (
+    <section className="py-[7.5rem] flex max-md:flex-col md:gap-24">
+      <img src={image} alt="WooCommerce Integration" className="shadow-md border-[0.625rem] rounded-[1.25rem] border-black" />
+    </section>
+  );
 }
 
 export default WooCommerceIntegration;
