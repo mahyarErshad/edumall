0698163d891cb7145776966bbdc58fa1d8c92e8e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-10 12:30:46 +0430

    Added tutor image

--- src/Components/Body/PremiumPlugins/PremiumPlugins.jsx
+++ src/Components/Body/PremiumPlugins/PremiumPlugins.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function PremiumPlugins() {
   return (
-    <section className='py-[7.5rem] bg-navy flex max-md:flex-col w-full px-4'>
-    <div className='md:w-2/4 max-md:w-full flex-center'>
-        <h3 className='text-white font-medium max-md:text-[1.75rem] md:text-5xl'>ادغام پلاگین های پریمیوم</h3>
-    </div>
-    <div className='md:w-2/4 max-md:w-full flex-center'></div>
+    <section className="py-[7.5rem] bg-navy flex max-md:flex-col w-full px-4">
+      <div className="md:w-2/4 max-md:w-full flex-center flex-col gap-9">
+        <h3 className="text-white font-medium max-md:text-[1.75rem] md:text-5xl">ادغام پلاگین های پریمیوم</h3>
+        <p className="text-base text-[#FFFFFFCC] md:max-w-[33.75rem]">فقط مناسب ترین افزونه ها در اجومال ادغام شده اند تا به کاربران کمک کنند تا وب سایت های آموزشی قدرتمند و کاملاً کاربردی بسازند.</p>
+      </div>
+      <div className="md:w-2/4 max-md:w-full flex-center"></div>
     </section>
-  )
+  );
 }
 
-export default PremiumPlugins
+export default PremiumPlugins;
