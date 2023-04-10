c8ffc4707b53cf762cac78cfed43765bd34d82ea
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-10 12:13:59 +0430

    Commponent is responsive

--- src/Components/Body/WooCommerceIntegration/WooCommerceIntegrationCard.jsx
+++ src/Components/Body/WooCommerceIntegration/WooCommerceIntegrationCard.jsx
@@ -4,7 +4,7 @@ import styles from "../ContentDrip/style.module.css"
 
 function WooCommerceIntegrationCard(props) {
   return (
-    <figure className="md:flex-center max-md:flex md:flex-col gap-3 max-md:w-full md:max-w-[15.625rem] max-md:mt-4">
+    <figure className="md:flex-center max-md:flex md:flex-col gap-3 w-full max-md:mt-4">
       <div className="flex-center md:w-full max-md:flex-col">
         <span className="font-medium w-12 h-12 flex-center rounded-[20px] bg-[#f1f3fa] text-primary">{props.counter}</span>
         <span className={`flex-grow h-1 ${props.hasDots && styles['background-infinite-dots']} max-md:mt-4 max-md:bg-repeat-y md:bg-repeat-x max-md:h-8 max-md:w-2 md:w-full`}></span>
