e826d377199141653a016ca5f1fb2ea3097a51aa
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-10 15:51:02 +0430

    Woocommerce is in a strong tag

--- src/Components/Body/WooCommerceIntegration/WooCommerceIntegration.jsx
+++ src/Components/Body/WooCommerceIntegration/WooCommerceIntegration.jsx
@@ -23,7 +23,9 @@ function WooCommerceIntegration() {
         <img src={image} alt="WooCommerce Integration" className="shadow-md border-[0.625rem] rounded-[1.25rem] border-black self-start" />
       </div>
       <div className="max-md:w-full md:w-2/4 flex flex-col justify-start items-start">
-        <h3 className="font-medium max-md:text-[1.75rem] md:text-5xl line_after mb-[4.6875rem]">ادغام ووکامرس</h3>
+        <h3 className="font-medium max-md:text-[1.75rem] md:text-5xl mb-[4.6875rem]">
+          ادغام <strong className="line_after">ووکامرس</strong>
+        </h3>
         <div className="w-full flex flex-col">
           <WooCommerceIntegrationCard counter="۰۱" text="فروش هر چیزی از محصولات دیجیتال گرفته تا فیزیکی با افزونه ووکامرس." />
           <WooCommerceIntegrationCard counter="۰۲" text="راه اندازی سیستم پرداخت با محبوب ترین روش های پرداخت: پی پال، استریپ، حواله بانکی." />
