6cfce299e6fd3d25d24eca3839cc41e1fb6eb919
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-10 12:09:26 +0430

    first and second cards have dots

--- src/Components/Body/WooCommerceIntegration/WooCommerceIntegration.jsx
+++ src/Components/Body/WooCommerceIntegration/WooCommerceIntegration.jsx
@@ -11,8 +11,8 @@ function WooCommerceIntegration() {
       <div className="max-md:w-full md:w-2/4 flex flex-col justify-start items-start">
         <h3 className="font-medium max-md:text-[1.75rem] md:text-5xl line_after mb-[4.6875rem]">ادغام ووکامرس</h3>
         <div className="w-full flex flex-col gap-2">
-          <WooCommerceIntegrationCard counter="۰۱" text="بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید." />
-          <WooCommerceIntegrationCard counter="۰۲" text="دانش آموزان تا پایان درس قبلی نمی توانند به درس بعدی دسترسی داشته باشند." />
+          <WooCommerceIntegrationCard counter="۰۱" text="بخش ها و ترتیب درس های خود را تنظیم و سفارشی کنید." hasDots />
+          <WooCommerceIntegrationCard counter="۰۲" text="دانش آموزان تا پایان درس قبلی نمی توانند به درس بعدی دسترسی داشته باشند." hasDots />
           <WooCommerceIntegrationCard counter="۰۳" text="مدیر، مربیان می توانند درس را تا زمان شروع قفل کنند." />
         </div>
       </div>
