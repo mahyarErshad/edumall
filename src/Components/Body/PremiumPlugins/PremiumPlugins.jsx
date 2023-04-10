964ed3fff75ee9fff7e4c8db7afb5fa5150214e2
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-10 12:26:56 +0430

    h3 is added

--- src/Components/Body/PremiumPlugins/PremiumPlugins.jsx
+++ src/Components/Body/PremiumPlugins/PremiumPlugins.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function PremiumPlugins() {
   return (
-    <div>PremiumPlugins</div>
+    <section className='py-[7.5rem] bg-navy flex max-md:flex-col w-full px-4'>
+    <div className='md:w-2/4 max-md:w-full flex-center'>
+        <h3 className='text-white font-medium max-md:text-[1.75rem] md:text-5xl'>ادغام پلاگین های پریمیوم</h3>
+    </div>
+    <div className='md:w-2/4 max-md:w-full flex-center'></div>
+    </section>
   )
 }
 
