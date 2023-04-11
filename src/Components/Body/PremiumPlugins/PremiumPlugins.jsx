24ec129fb9bd214a53009aa25aaac9cfd17dcf57
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-11 10:38:14 +0430

    Premium has line_after class

--- src/Components/Body/PremiumPlugins/PremiumPlugins.jsx
+++ src/Components/Body/PremiumPlugins/PremiumPlugins.jsx
@@ -13,7 +13,9 @@ function PremiumPlugins() {
   return (
     <section className="py-[7.5rem] bg-navy flex max-lg:flex-col max-lg:gap-12 w-full px-4">
       <div className="lg:w-2/4 max-lg:w-full flex justify-start max-lg:items-center lg:items-center flex-col gap-9">
-        <h3 className="text-white font-medium max-md:text-[1.75rem] md:text-5xl">ادغام پلاگین های پریمیوم</h3>
+        <h3 className="text-white font-medium max-md:text-[1.75rem] md:text-5xl">
+          ادغام پلاگین های <span className="line_after">حرفه ای</span>
+        </h3>
         <p className="text-base text-[#FFFFFFCC] md:max-w-[33.75rem]">فقط مناسب ترین افزونه ها در اجومال ادغام شده اند تا به کاربران کمک کنند تا وب سایت های آموزشی قدرتمند و کاملاً کاربردی بسازند.</p>
       </div>
       <div className="lg:w-2/4 max-lg:w-full flex-center flex-wrap gap-12">
