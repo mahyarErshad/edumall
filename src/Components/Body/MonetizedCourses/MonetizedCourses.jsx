5ca373f300a22775ad95997005b346ea2bb43e8b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-13 09:11:36 +0430

    Given id "earnings" to this section

--- src/Components/Body/MonetizedCourses/MonetizedCourses.jsx
+++ src/Components/Body/MonetizedCourses/MonetizedCourses.jsx
@@ -3,7 +3,7 @@ import MonetizedCoursesTabs from "./MonetizedCoursesTabs";
 
 function MonetizedCourses() {
   return (
-    <section className="w-full px-4 bg-[#F5F7FD] py-24 flex justify-start items-center flex-col gap-8 overflow-hidden">
+    <section id="earnings" className="w-full px-4 bg-[#F5F7FD] py-24 flex justify-start items-center flex-col gap-8 overflow-hidden">
       <h3 className="max-md:text-3xl md:text-5xl font-medium">
         <span className="line_after">درآمد</span> از دوره های آموزشی
       </h3>
