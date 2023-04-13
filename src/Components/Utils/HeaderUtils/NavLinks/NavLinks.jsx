b0d2516d803e94f86d46afc682d3174a8d4a0abd
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-04-13 09:09:57 +0430

    Changed the text and the href of second NavLink

--- src/Components/Utils/HeaderUtils/NavLinks/NavLinks.jsx
+++ src/Components/Utils/HeaderUtils/NavLinks/NavLinks.jsx
@@ -5,7 +5,7 @@ function NavLinks({className}) {
   return (
     <>
       <NavLink className={className} text="ویژگی ها" href="#features" />
-      <NavLink className={className} text="ویدئوهای آموزشی" href="#tutorials" />
+      <NavLink className={className} text="کسب درآمد" href="#earnings" />
       <NavLink className={className} text="دموها" href="#demos" />
       <NavLink className={className} text="دوره ها" href="#courses" />
     </>
